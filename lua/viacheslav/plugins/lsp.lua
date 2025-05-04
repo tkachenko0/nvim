d3323656978618f16ce7937344f70c61b97a1386
Viacheslav Tkachenko <t.viacheslav00@gmail.com>
2025-05-05 01:15:23 +0200

    batman

--- lua/viacheslav/plugins/lsp.lua
+++ lua/viacheslav/plugins/lsp.lua
@@ -152,6 +152,15 @@ return {
             update_in_insert = true,
             float = { source = "always" },
         })
+
+        -- Change the Diagnostic symbols in the sign column (gutter)
+		-- (not in youtube nvim video)
+		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		end
+
     end,
 }
 
