56a2c06f26855db97fa2566b41f69161ad707237
Viacheslav Tkachenko <t.viacheslav00@gmail.com>
2025-05-12 00:39:59 +0200

    addded docker and yaml lsps

--- lua/viacheslav/plugins/harpoon.lua
+++ lua/viacheslav/plugins/harpoon.lua
@@ -1,24 +1,21 @@
-return {
-  "ThePrimeagen/harpoon",
-  branch = "harpoon2",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    local harpoon = require("harpoon")
-
-    -- REQUIRED SETUP
-    harpoon:setup()
-
-    -- Mappings for QWERTY users
-    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-    vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-    vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
-    vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
-
-    -- Previous/next navigation in the Harpoon list
-    vim.keymap.set("n", "<C-S-p>", function() harpoon:list():prev() end)
-    vim.keymap.set("n", "<C-S-n>", function() harpoon:list():next() end)
-  end,
-}
+return {}
+-- return {
+--   "ThePrimeagen/harpoon",
+--   branch = "harpoon2",
+--   dependencies = { "nvim-lua/plenary.nvim" },
+--   config = function()
+--     local harpoon = require("harpoon")
+--     -- REQUIRED SETUP
+--     harpoon:setup()
+--     -- Mappings for QWERTY users
+--     vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+--     vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+--     vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+--     vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
+--     vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
+--     vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
+--     -- Previous/next navigation in the Harpoon list
+--     vim.keymap.set("n", "<C-S-p>", function() harpoon:list():prev() end)
+--     vim.keymap.set("n", "<C-S-n>", function() harpoon:list():next() end)
+--   end,
+-- }
--- lua/viacheslav/plugins/lualine.rc.lua
+++ lua/viacheslav/plugins/lualine.rc.lua
@@ -1,51 +1,53 @@
-return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        local diagnostics_config = {
-            'diagnostics',
-            sources = { "nvim_diagnostic" },
-            symbols = {
-                error = ' ', warn = ' ', info = ' ', hint = ' '
-            }
-        }
-        local filename_config = {
-            'filename',
-            file_status = true, -- shows file status (readonly, modified)
-            path = 0            -- 0 = just filename, 1 = relative path, 2 = absolute path
-        }
-        require("lualine").setup {
-            options = {
-                icons_enabled = true,
-                theme = 'vscode',
-                section_separators = { left = '', right = '' },
-                component_separators = { left = '', right = '' },
-                disabled_filetypes = {}
-            },
-            sections = {
-                lualine_a = { 'mode' },
-                lualine_b = { 'branch' },
-                lualine_c = { filename_config },
-                lualine_x = { diagnostics_config, 'encoding', 'filetype' },
-                lualine_y = { 'progress' },
-                lualine_z = { 'location' }
-            },
-            inactive_sections = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = {
-                    {
-                        'filename',
-                        file_status = true,
-                        path = 1 -- Show relative path in inactive sections
-                    }
-                },
-                lualine_x = { 'location' },
-                lualine_y = {},
-                lualine_z = {}
-            },
-            tabline = {},
-            extensions = { 'fugitive' }
-        }
-    end
-}
+vim.o.laststatus = 0
+return {}
+-- return {
+--     "nvim-lualine/lualine.nvim",
+--     dependencies = { "nvim-tree/nvim-web-devicons" },
+--     config = function()
+--         local diagnostics_config = {
+--             'diagnostics',
+--             sources = { "nvim_diagnostic" },
+--             symbols = {
+--                 error = ' ', warn = ' ', info = ' ', hint = ' '
+--             }
+--         }
+--         local filename_config = {
+--             'filename',
+--             file_status = true, -- shows file status (readonly, modified)
+--             path = 0            -- 0 = just filename, 1 = relative path, 2 = absolute path
+--         }
+--         require("lualine").setup {
+--             options = {
+--                 icons_enabled = true,
+--                 theme = 'vscode',
+--                 section_separators = { left = '', right = '' },
+--                 component_separators = { left = '', right = '' },
+--                 disabled_filetypes = {}
+--             },
+--             sections = {
+--                 lualine_a = { 'mode' },
+--                 lualine_b = { 'branch' },
+--                 lualine_c = { filename_config },
+--                 lualine_x = { diagnostics_config, 'encoding', 'filetype' },
+--                 lualine_y = { 'progress' },
+--                 lualine_z = { 'location' }
+--             },
+--             inactive_sections = {
+--                 lualine_a = {},
+--                 lualine_b = {},
+--                 lualine_c = {
+--                     {
+--                         'filename',
+--                         file_status = true,
+--                         path = 1 -- Show relative path in inactive sections
+--                     }
+--                 },
+--                 lualine_x = { 'location' },
+--                 lualine_y = {},
+--                 lualine_z = {}
+--             },
+--             tabline = {},
+--             extensions = { 'fugitive' }
+--         }
+--     end
+-- }
