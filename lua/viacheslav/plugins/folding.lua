a83e372873e25d1cc8e2fbdc15eac800bb0df6ed
Viacheslav Tkachenko <t.viacheslav00@gmail.com>
2025-05-11 18:42:50 +0200

    addded docker and yaml lsps

--- lua/viacheslav/plugins/folding.lua
+++ lua/viacheslav/plugins/folding.lua
@@ -1,24 +1,35 @@
 return {
-  'kevinhwang91/nvim-ufo',
-  dependencies = { 'kevinhwang91/promise-async' },
-  event = 'BufReadPost',  -- Load when a buffer is read
-  config = function()
-    -- Basic Vim settings for folding
-    vim.o.foldcolumn = '1'
-    vim.o.foldlevel = 99
-    vim.o.foldlevelstart = 99
-    vim.o.foldenable = true
+  {
+    "kevinhwang91/nvim-ufo",
+    event = "BufRead",
+    dependencies = {
+      { "kevinhwang91/promise-async" },
+      {
+        "luukvbaal/statuscol.nvim",
+        config = function()
+          local builtin = require("statuscol.builtin")
+          require("statuscol").setup({
+            -- foldfunc = "builtin",
+            -- setopt = true,
+            relculright = true,
+            segments = {
+              { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+              { text = { "%s" }, click = "v:lua.ScSa" },
+              { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+            },
+          })
+        end,
+      },
+    },
+    config = function()
+      -- Fold options
+      vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+      vim.o.foldcolumn = "1" -- '0' is not bad
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
 
-    -- Keymaps for folding operations
-    vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-    vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-
-    -- Setup for ufo using LSP + indent as fallback
-    require('ufo').setup({
-      provider_selector = function()
-        return { 'lsp', 'indent' }
-      end,
-    })
-  end
+      require("ufo").setup()
+    end,
+  },
 }
-
