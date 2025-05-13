388603fa21a71e681bdbfc56699df606a3172e6f
Viacheslav Tkachenko <t.viacheslav00@gmail.com>
2025-05-13 07:26:23 +0200

    addded docker and yaml lsps

--- lua/viacheslav/plugins/lsp.lua
+++ lua/viacheslav/plugins/lsp.lua
@@ -43,7 +43,6 @@ return {
 			callback = function(e)
                 local opts = { buffer = e.buf }
                 vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-                -- vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
                 vim.keymap.set("n", "K", function() vim.lsp.buf.hover { border = "single" } end, opts)
                 vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
                 vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
