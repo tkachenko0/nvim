cc8a272ab69fad86919fd4917ed142056688ca9e
viacheslav.tkachenko <t.viacheslav00@gmail.com>
2025-05-13 22:51:08 +0200

    configs

--- lua/viacheslav/core/remap.lua
+++ lua/viacheslav/core/remap.lua
@@ -11,8 +11,6 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set('n', '<Tab>', '<C-6>', { noremap = true, silent = true })
-
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
@@ -23,9 +21,9 @@ vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>")
 vim.keymap.set("n", "x", '"_x', { silent = true })
 vim.keymap.set("n", "d", '"_d', { silent = true })
 vim.keymap.set("n", "c", '"_c', { silent = true })
+vim.keymap.set("n", "s", '"_s', { silent = true })
+vim.keymap.set("v", "s", '"_s', { silent = true })
 
 vim.keymap.set("n", "gg", "gg0")
 vim.keymap.set("n", "G", "G$")
 vim.keymap.set("v", "G", "G$")
-
-vim.keymap.set("n", "<leader>q", ":bd<CR>", { desc = "Close buffer" })
--- lua/viacheslav/plugins/filetree.lua
+++ lua/viacheslav/plugins/filetree.lua
@@ -1,86 +1,86 @@
+local current_source_index = 1
+local sources = { "filesystem", "buffers", "git_status" }
+
+local function cycle_neo_tree_source(state)
+  current_source_index = current_source_index % #sources + 1
+  local source = sources[current_source_index]
+  require("neo-tree.command").execute({
+    source = source,
+    toggle = false,
+    reveal = true,
+  })
+end
+
 return {
-    {
-        "nvim-tree/nvim-tree.lua",
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",           -- Required for neo-tree
+    "nvim-tree/nvim-web-devicons",     -- Optional, but provides file icons
+    "MunifTanjim/nui.nvim",            -- Required for neo-tree UI
+  },
+  cmd = "Neotree",
+  keys = {
+    { "<leader>e", "<cmd>Neotree filesystem reveal toggle<CR>", desc = "Toggle Filesystem Tree" },
+    { "<leader>b", "<cmd>Neotree buffers reveal toggle right<CR>", desc = "Toggle Buffers Tree" },
+    { "<leader>gs", "<cmd>Neotree git_status reveal toggle left<CR>", desc = "Toggle Git Status View" },
+  },
+  config = function()
+    require("neo-tree").setup({
+      sources = { "filesystem", "buffers", "git_status" },
+      source_selector = {
+        winbar = true,
+        sources = {
+          { source = "filesystem", display_name = "󰉓 Files" },
+          { source = "buffers",    display_name = "󰈚 Buffers" },
+          { source = "git_status", display_name = "󰊢 Git" },
         },
-        config = function()
-            local function on_attach(bufnr)
-                local api = require("nvim-tree.api")
-
-                local opts = function(desc)
-                    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-                end
-
-                -- Default mappings
-                api.config.mappings.default_on_attach(bufnr)
-
-                -- Custom mappings
-                vim.keymap.set("n", "<Right>", api.node.open.edit, opts("Open Node"))
-                vim.keymap.set("n", "<Left>", api.node.navigate.parent_close, opts("Close Parent Node"))
-            end
-
-            require("nvim-tree").setup({
-                sort_by = "case_sensitive",
-                view = {
-                    width = 35,
-                    side = "left",
-                    adaptive_size = true,
-                    preserve_window_proportions = true,
-                    number = false,
-                    relativenumber = false,
-                    signcolumn = "yes",
-                },
-                renderer = {
-                    group_empty = true,
-                    root_folder_label = false,
-                    highlight_git = true,
-                    indent_markers = {
-                        enable = true,
-                    },
-                    icons = {
-                        glyphs = {
-                            default = "󰈚",
-                            folder = {
-                                default = "",
-                                empty = "",
-                                empty_open = "",
-                                open = "",
-                                symlink = "",
-                            },
-                            git = { unmerged = "" },
-                        },
-                    },
-                },
-                filters = {
-                    dotfiles = false,
-                    custom = {},
-                },
-                git = {
-                    enable = true,
-                    ignore = false,
-                },
-                diagnostics = {
-                    enable = true,
-                    show_on_dirs = true,
-                    icons = {
-                        hint = "",
-                        info = "",
-                        warning = "",
-                        error = "",
-                    },
-                },
-                actions = {
-                    open_file = {
-                        resize_window = true,
-                    },
-                },
-                update_focused_file = {
-                    enable = true,
-                    update_cwd = true,
-                },
-                on_attach = on_attach, -- ← important line
-            })
-        end,
-    },
+      },
+      window = {
+        position = "left",
+        mappings = {
+          ["<cr>"] = "open",
+          ["<Right>"] = "open",
+          ["<Left>"] = "close_node",
+          ["<Tab>"] = cycle_neo_tree_source,
+          ["P"] = {
+            "toggle_preview",
+            config = {
+              use_float = false,
+              use_image_nvim = true,
+              title = 'Neo-tree Preview',
+            },
+          },
+        },
+      },
+      buffers = {
+        follow_current_file = {
+          enabled = true,
+        },
+        show_hidden = true,
+      },
+      filesystem = {
+        follow_current_file = {
+          enabled = true,
+        },
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+        use_libuv_file_watcher = true,
+      },
+      git_status = {
+        follow_current_file = {
+          enabled = true,
+        },
+        window = {
+          mappings = {
+          },
+        },
+      },
+    })
+  end,
 }
+
