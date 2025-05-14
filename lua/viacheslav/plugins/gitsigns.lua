048aea637ae6750e8d880fec772d28a2419f0019
viacheslav.tkachenko <t.viacheslav00@gmail.com>
2025-05-14 09:02:59 +0200

    gutsigns

--- lua/viacheslav/plugins/gitsigns.lua
+++ lua/viacheslav/plugins/gitsigns.lua
@@ -2,15 +2,30 @@ return {
 	"lewis6991/gitsigns.nvim",
 	event = { "BufReadPre", "BufNewFile" },
 	opts = {
+		current_line_blame = true,
+		current_line_blame_opts = {
+			virt_text = true,
+			virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+			delay = 1000,
+			ignore_whitespace = false,
+			virt_text_priority = 100,
+			use_focus = true,
+		  },
+		preview_config = {
+			-- Options passed to nvim_open_win
+			border = "rounded",
+			style = 'minimal',
+			relative = 'cursor',
+			row = 0,
+			col = 1
+		  },
 		on_attach = function(bufnr)
 			local gs = package.loaded.gitsigns
 			local function map(mode, l, r, desc)
 				vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
 			end
-			-- Navigation
 			map("n", "]h", gs.next_hunk, "Next Hunk")
 			map("n", "[h", gs.prev_hunk, "Prev Hunk")
-			-- Actions
 			map("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
 			map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
 			map("v", "<leader>hs", function()
