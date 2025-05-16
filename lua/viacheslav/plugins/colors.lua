d98b3e26eede35d364b508b3a62b1f2376fef23b
viacheslav.tkachenko <t.viacheslav00@gmail.com>
2025-05-16 17:20:49 +0200

    colors

--- lua/viacheslav/plugins/colors.lua
+++ lua/viacheslav/plugins/colors.lua
@@ -11,16 +11,3 @@ return {
         end,
     }
 }
-
--- jetbrains color scheme 
--- return {
---     "nickkadutskyi/jb.nvim",
---     lazy = false,
---     priority = 1000,
---     opts = {},
---     config = function()
---         -- require("jb").setup({transparent = true})
---         vim.cmd("colorscheme jb")
---     end,
--- }
-
