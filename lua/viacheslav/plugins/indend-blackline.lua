43f6f7cb405406af97b7912e0c8a082794ce7ffb
viacheslav.tkachenko <t.viacheslav00@gmail.com>
2025-05-17 04:00:28 +0200

    configs

--- lua/viacheslav/plugins/indend-blackline.lua
+++ lua/viacheslav/plugins/indend-blackline.lua
@@ -1,13 +1,26 @@
 return {
-	"lukas-reineke/indent-blankline.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	main = "ibl",
-	opts = {
-		indent = { char = "┊" },
-    scope = {
-      enabled = true, -- turn on to show scope guides
+  'lukas-reineke/indent-blankline.nvim',
+  main = 'ibl',
+  opts = {
+    indent = {
+      char = '▏',
     },
-	},
+    scope = {
+      show_start = false,
+      show_end = false,
+      show_exact_scope = false,
+    },
+    exclude = {
+      filetypes = {
+        'help',
+        'startify',
+        'dashboard',
+        'packer',
+        'neogitstatus',
+        'NvimTree',
+        'Trouble',
+      },
+    },
+  },
 }
-
 
