f747d33d7d3e0f941dda33ac2ee1514a665e7e7f
viacheslav.tkachenko <t.viacheslav00@gmail.com>
2025-05-17 04:13:10 +0200

    configs

--- lua/viacheslav/plugins/colorizer.lua
+++ lua/viacheslav/plugins/colorizer.lua
@@ -1,15 +1,11 @@
 return {
     "norcalli/nvim-colorizer.lua",  -- Colorizer plugin
     config = function()
-        -- Set up colorizer for all filetypes
         require("colorizer").setup({
             "*",  -- Apply to all filetypes
             css = { rgb_fn = true },  -- Support RGB functions in CSS
             html = { names = true },  -- Support color names in HTML
         })
-
-        -- Optionally, bind a key to toggle colorizer
-        vim.api.nvim_set_keymap('n', '<leader>tc', ':ColorizerToggle<CR>', { noremap = true, silent = true })
     end
 }
 
