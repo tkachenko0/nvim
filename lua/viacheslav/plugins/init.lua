cb664754ebca717257fa9c3dfd3f108ed9858c80
viacheslav.tkachenko <t.viacheslav00@gmail.com>
2025-05-17 04:12:05 +0200

    configs

--- lua/viacheslav/plugins/init.lua
+++ lua/viacheslav/plugins/init.lua
@@ -6,4 +6,31 @@ return {
     "github/copilot.vim",
     "eandrju/cellular-automaton.nvim",
     "gpanders/editorconfig.nvim",
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = function()
+            require("nvim-autopairs").setup()
+        end,
+    },
+    {
+    "laytan/cloak.nvim",
+    config = function()
+        require("cloak").setup({
+            enabled = true,
+            cloak_character = "*",
+            highlight_group = "Comment",
+            patterns = {
+                {
+                    file_pattern = {
+                        ".env*",
+                        "wrangler.toml",
+                        ".dev.vars",
+                    },
+                    cloak_pattern = "=.+"
+                },
+            },
+        })
+    end
+}
 }
