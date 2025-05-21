d13649cc6e7a9c1ec2fdfa125207a734b55ef03b
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-05-21 22:16:38 +0200

    added check on processed

--- lua/viacheslav/plugins/outline.lua
+++ lua/viacheslav/plugins/outline.lua
@@ -6,41 +6,26 @@ return {
   },
   keys = {
     { "<leader>a", "<cmd>AerialToggle!<CR>", desc = "Toggle Aerial outline" },
-    { "{",         "<cmd>AerialPrev<CR>",    desc = "Aerial: Previous symbol" },
-    { "}",         "<cmd>AerialNext<CR>",    desc = "Aerial: Next symbol" },
   },
   opts = {
+    lazy_load = true,
     on_attach = function(bufnr)
-      vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr, desc = "Aerial: Previous symbol" })
-      vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr, desc = "Aerial: Next symbol" })
+      vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr, desc = "Previous symbol" })
+      vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr, desc = "Next symbol" })
     end,
     backends = { "treesitter", "lsp", "markdown", "asciidoc", "man" },
     layout = {
-      max_width = { 40, 0.2 },
-      min_width = 10,
       default_direction = "prefer_right",
-      placement = "window",
-      resize_to_content = true,
-      preserve_equality = false,
     },
-    attach_mode = "window",
-    close_automatic_events = {},
-    lazy_load = true,
-    disable_max_lines = 10000,
-    disable_max_size = 2000000,
-    filter_kind = {
-      "Class",
-      "Constructor",
-      "Enum",
-      "Function",
-      "Interface",
-      "Module",
-      "Method",
-      "Struct",
-    },
+    attach_mode = "global",
     highlight_mode = "split_width",
     highlight_closest = true,
-    highlight_on_hover = false,
+    highlight_on_hover = true,
     highlight_on_jump = 300,
+    show_guides = true,
+    float = {
+      border = "rounded",
+      relative = "cursor",
+    },
   },
 }
