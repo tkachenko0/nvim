7f6edeecc22f29916188564529170ec4b18562f7
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-05-23 15:57:24 +0200

    lualine

--- lua/viacheslav/plugins/lualine.rc.lua
+++ lua/viacheslav/plugins/lualine.rc.lua
@@ -1,39 +1,3 @@
--- function _G.GitBranch()
---     local handle = io.popen("git -C " .. vim.fn.expand("%:p:h") .. " rev-parse --abbrev-ref HEAD 2>/dev/null")
---     if handle then
---         local branch = handle:read("*l")
---         handle:close()
---         return branch and " " .. branch or ""
---     end
---     return ""
--- end
---
--- local signs = { error = ' ', warn = ' ', info = ' ', hint = ' ' }
---
--- function Diagnostics()
---     local bufnr = 0 -- current buffer
---     local result = {}
---
---     for type, icon in pairs(signs) do
---         local n = #vim.diagnostic.get(bufnr, { severity = vim.diagnostic.severity[type:upper()] })
---         if n > 0 then
---             table.insert(result, icon .. n)
---         end
---     end
---
---     return table.concat(result, ' ')
--- end
---
--- vim.o.statusline =
---     "%{v:lua.GitBranch()}    " .. -- Git branch
---     "%<%f %m%r" ..                -- File path and flags
---     "%=" ..                       -- Right align
---     -- "%l:%c " ..                   -- Line, Column (optional)
---     " %{v:lua.Diagnostics()} " .. -- Diagnostics
---     "%y"                          -- Filetype
-
-
--- return {}
 return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -47,8 +11,8 @@ return {
         }
         local filename_config = {
             'filename',
-            file_status = true, -- shows file status (readonly, modified)
-            path = 0            -- 0 = just filename, 1 = relative path, 2 = absolute path
+            file_status = true,
+            path = 0 -- 0 = just filename, 1 = relative path, 2 = absolute path
         }
         require("lualine").setup {
             options = {
