44dd8b024a689d7bb60cbf61d46df1c98eff87b8
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-05-23 16:28:46 +0200

    lualine

--- lua/viacheslav/plugins/outline.lua
+++ lua/viacheslav/plugins/outline.lua
@@ -9,14 +9,7 @@ return {
   },
   opts = {
     lazy_load = true,
-    on_attach = function(bufnr)
-      vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr, desc = "Previous symbol" })
-      vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr, desc = "Next symbol" })
-    end,
     backends = { "treesitter", "lsp", "markdown", "asciidoc", "man" },
-    layout = {
-      default_direction = "prefer_right",
-    },
     attach_mode = "global",
     highlight_mode = "split_width",
     highlight_closest = true,
