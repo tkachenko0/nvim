464c7168941e74c5cb9ca3f67dc0592d0b9231aa
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-05-23 16:32:21 +0200

    lualine

--- lua/viacheslav/plugins/gitsigns.lua
+++ lua/viacheslav/plugins/gitsigns.lua
@@ -5,14 +5,12 @@ return {
 		current_line_blame = true,
 		current_line_blame_opts = {
 			virt_text = true,
-			virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
 			delay = 0,
 			ignore_whitespace = false,
 			virt_text_priority = 100,
 			use_focus = true,
 		},
 		preview_config = {
-			-- Options passed to nvim_open_win
 			border = "rounded",
 			style = 'minimal',
 			relative = 'cursor',
@@ -27,26 +25,14 @@ return {
 			map("n", "]h", gs.next_hunk, "Next Hunk")
 			map("n", "[h", gs.prev_hunk, "Prev Hunk")
 			map("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
-			map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
-			map("v", "<leader>hs", function()
-				gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-			end, "Stage hunk")
-			map("v", "<leader>hr", function()
-				gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-			end, "Reset hunk")
 			map("n", "<leader>hS", gs.stage_buffer, "Stage buffer")
+			map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
 			map("n", "<leader>hR", gs.reset_buffer, "Reset buffer")
 			map("n", "<leader>hu", gs.undo_stage_hunk, "Undo stage hunk")
-			map("n", "<leader>hp", gs.preview_hunk, "Preview hunk")
-			map("n", "<leader>hb", function()
-				gs.blame_line({ full = true })
-			end, "Blame line")
-			map("n", "<leader>hB", gs.toggle_current_line_blame, "Toggle line blame")
 			map("n", "<leader>hd", gs.diffthis, "Diff this")
 			map("n", "<leader>hD", function()
 				gs.diffthis("~")
 			end, "Diff this ~")
-			map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "Gitsigns select hunk")
 		end,
 	},
 }
