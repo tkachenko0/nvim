5093151045a7d28702ddfa22ee94cb6b80b5f7f1
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-05-23 16:12:32 +0200

    lualine

--- lua/viacheslav/core/init.lua
+++ lua/viacheslav/core/init.lua
@@ -6,12 +6,6 @@ local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
 
-vim.filetype.add({
-    extension = {
-        templ = 'templ',
-    }
-})
-
 autocmd('TextYankPost', {
     group = yank_group,
     pattern = '*',
