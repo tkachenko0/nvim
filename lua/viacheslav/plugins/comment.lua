d1ec1abb7e2b373d40d22c27c32c2044d8b36a3e
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-05-23 16:03:37 +0200

    lualine

--- lua/viacheslav/plugins/comment.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'numToStr/Comment.nvim',
-  opts = {},
-  config = function()
-    local opts = { noremap = true, silent = true }
-    vim.keymap.set('n', '<C-/>', require('Comment.api').toggle.linewise.current, opts)
-  end,
-}
