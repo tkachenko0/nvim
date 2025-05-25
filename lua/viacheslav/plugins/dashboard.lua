03584e96579f9e56c0864d359ef6eaa8a881dcd4
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-05-25 23:39:05 +0200

    init

--- lua/viacheslav/plugins/dashboard.lua
+++ lua/viacheslav/plugins/dashboard.lua
@@ -1,6 +1,7 @@
 return {
   'nvimdev/dashboard-nvim',
   event = 'VimEnter',
+  dependencies = { { 'nvim-tree/nvim-web-devicons' } },
   config = function()
     require('dashboard').setup {
       theme = 'hyper',
@@ -19,5 +20,4 @@ return {
       },
     }
   end,
-  dependencies = { { 'nvim-tree/nvim-web-devicons' } }
 }
