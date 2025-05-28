335ca17a937b7ce6afdb8a34194a75a9f8897efb
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-05-28 21:33:02 +0200

    feat(doc) README.md

---
+++ lua/viacheslav/plugins/dashboard.lua
@@ -0,0 +1,23 @@
+return {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+    config = function()
+        require('dashboard').setup {
+            theme = 'hyper',
+            config = {
+                project = { enable = false },
+                header = {
+                    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+                    "⠀⠀⠀⠀⠀⠀¯\\_(ツ)_/¯⠀⠀⠀⠀⠀⠀",
+                },
+                footer = {},
+                shortcut = {},
+                mru = {
+                    limit = 10,
+                    cwd_only = true,
+                },
+            },
+        }
+    end,
+}
