d884696acd782dc162208e141e5509efd20c72df
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-05-28 02:40:30 +0200

    feat(upload): section control

--- lua/viacheslav/core/remap.lua
+++ lua/viacheslav/core/remap.lua
@@ -45,6 +45,9 @@ vim.keymap.set('v', '>', '>gv')
 vim.keymap.set('n', '<Tab>', '<C-6>', { noremap = true, silent = true })
 vim.keymap.set("n", "Q", ":qa<CR>", { desc = "Quit all" })
 
+-- Copy athe whole file
+vim.keymap.set("n", "<C-c>", "<cmd>%y+<CR>")
+
 -- Cycle through normal buffers with Alt+Left and Alt+Right
 vim.keymap.set('n', '<A-Left>', function()
     local bufnrs = vim.tbl_filter(function(b)
