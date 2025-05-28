efba3b0ff256d89b0b02781602c097f15a46fb2d
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-05-28 09:23:28 +0200

    feat(upload): section control

--- lua/viacheslav/plugins/init.lua
+++ lua/viacheslav/plugins/init.lua
@@ -37,9 +37,8 @@ return {
         "norcalli/nvim-colorizer.lua",
         config = function()
             require("colorizer").setup({
-                "*",
-                css = { rgb_fn = true },
-                html = { names = true },
+                "css",
+                "scss",
             })
         end
     },
