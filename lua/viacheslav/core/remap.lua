9db4361ebf6e04b8c6e49559dbd28e9f90e5d71c
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-05-28 02:45:36 +0200

    feat(upload): section control

--- lua/viacheslav/core/remap.lua
+++ lua/viacheslav/core/remap.lua
@@ -18,8 +18,6 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("i", "<C-c>", "<Esc>")
-
 -- Substitute
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
