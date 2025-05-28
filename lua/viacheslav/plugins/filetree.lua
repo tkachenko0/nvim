26b862f32053a1869a264ed541589b80d559d484
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-05-28 21:42:12 +0200

    feat(doc) README.md

--- lua/viacheslav/plugins/filetree.lua
+++ lua/viacheslav/plugins/filetree.lua
@@ -9,7 +9,7 @@ return {
     },
     cmd = "Neotree",
     keys = {
-      -- { "<leader>e", "<cmd>Neotree filesystem reveal toggle<CR>" },
+      { "<leader>e", "<cmd>Neotree filesystem reveal toggle<CR>" },
       { "<leader>b", "<cmd>Neotree buffers reveal toggle<CR>" },
     },
     config = function()
@@ -20,7 +20,7 @@ return {
           winbar = true,
           tabs_layout = "stacked",
           sources = {
-            -- { source = "filesystem", display_name = "󰉓 Files" },
+            { source = "filesystem", display_name = "󰉓 Files" },
             { source = "buffers", display_name = "󰈚 Buffers" },
             { source = "git_status", display_name = "󰊢 Git" },
           },
@@ -58,25 +58,25 @@ return {
           },
           show_hidden = true,
         },
-        -- filesystem = {
-        --   follow_current_file = {
-        --     enabled = true,
-        --     leave_dirs_open = true,
-        --     -- auto_expand_directories = false,
-        --   },
-        --   filtered_items = {
-        --     visible = true,
-        --     show_hidden_count = true,
-        --     hide_dotfiles = false,
-        --     hide_gitignored = false,
-        --     never_show = {
-        --       ".git",
-        --       ".DS_Store",
-        --       "__pycache__",
-        --     },
-        --   },
-        --   use_libuv_file_watcher = true,
-        -- },
+        filesystem = {
+          follow_current_file = {
+            enabled = true,
+            leave_dirs_open = true,
+            -- auto_expand_directories = false,
+          },
+          filtered_items = {
+            visible = true,
+            show_hidden_count = true,
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            never_show = {
+              ".git",
+              ".DS_Store",
+              "__pycache__",
+            },
+          },
+          use_libuv_file_watcher = true,
+        },
         git_status = {
           follow_current_file = {
             enabled = false,
@@ -90,74 +90,76 @@ return {
     end,
   },
 
-  {
-    {
-      "nvim-tree/nvim-tree.lua",
-      dependencies = {
-        "nvim-tree/nvim-web-devicons",
-      },
-      config = function()
-        local function on_attach(bufnr)
-          local api = require("nvim-tree.api")
-          local opts = function(desc)
-            return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-          end
-          api.config.mappings.default_on_attach(bufnr)
-          vim.keymap.set("n", "<Right>", api.node.open.edit, opts("Open Node"))
-          vim.keymap.set("n", "<Left>", api.node.navigate.parent_close, opts("Close Parent Node"))
-        end
-        require("nvim-tree").setup({
-          sort_by = "case_sensitive",
-          view = {
-            width = 35,
-            side = "left",
-            adaptive_size = true,
-            preserve_window_proportions = true,
-            number = false,
-            relativenumber = false,
-            signcolumn = "yes",
-          },
-          renderer = {
-            group_empty = true,
-            root_folder_label = false,
-            highlight_git = true,
-            indent_markers = {
-              enable = true,
-            },
-            icons = {
-              glyphs = {
-                default = "󰈚",
-                folder = {
-                  default = "",
-                  empty = "",
-                  empty_open = "",
-                  open = "",
-                  symlink = "",
-                },
-                git = { unmerged = "" },
-              },
-            },
-          },
-          filters = {
-            dotfiles = false,
-            custom = {},
-          },
-          git = {
-            enable = true,
-            ignore = false,
-          },
-          actions = {
-            open_file = {
-              resize_window = true,
-            },
-          },
-          update_focused_file = {
-            enable = true,
-            update_cwd = true,
-          },
-          on_attach = on_attach,
-        })
-      end,
-    },
-  }
+  -- {
+  --   {
+  --     "nvim-tree/nvim-tree.lua",
+  --     dependencies = {
+  --       "nvim-tree/nvim-web-devicons",
+  --     },
+  --     config = function()
+  --       local function on_attach(bufnr)
+  --         local api = require("nvim-tree.api")
+  --         local opts = function(desc)
+  --           return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  --         end
+  --         api.config.mappings.default_on_attach(bufnr)
+  --         vim.keymap.set("n", "<Right>", api.node.open.edit, opts("Open Node"))
+  --         vim.keymap.set("n", "<Left>", api.node.navigate.parent_close, opts("Close Parent Node"))
+  --       end
+  --       require("nvim-tree").setup({
+  --         sort_by = "case_sensitive",
+  --         view = {
+  --           width = 35,
+  --           side = "left",
+  --           adaptive_size = true,
+  --           preserve_window_proportions = true,
+  --           number = false,
+  --           relativenumber = false,
+  --           signcolumn = "yes",
+  --         },
+  --         renderer = {
+  --           group_empty = true,
+  --           root_folder_label = false,
+  --           highlight_git = true,
+  --           indent_markers = {
+  --             enable = true,
+  --           },
+  --           icons = {
+  --             glyphs = {
+  --               default = "󰈚",
+  --               folder = {
+  --                 default = "",
+  --                 empty = "",
+  --                 empty_open = "",
+  --                 open = "",
+  --                 symlink = "",
+  --               },
+  --               git = { unmerged = "" },
+  --             },
+  --           },
+  --         },
+  --         filters = {
+  --           dotfiles = false,
+  --           custom = {},
+  --         },
+  --         git = {
+  --           enable = true,
+  --           ignore = false,
+  --         },
+  --         actions = {
+  --           open_file = {
+  --             resize_window = true,
+  --           },
+  --         },
+  --         update_focused_file = {
+  --           enable = true,
+  --           update_cwd = true,
+  --         },
+  --         on_attach = on_attach,
+  --       })
+  --     end,
+  --   },
+  -- }
+  --
+  --
 }
