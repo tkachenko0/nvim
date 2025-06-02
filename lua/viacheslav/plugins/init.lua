bba9639fc9e9c39775e8e226cabccfdf5411f56f
viacheslav.tkachenko <t.viacheslav00@gmail.com>
2025-06-02 23:10:04 +0200

    auto tag

--- lua/viacheslav/plugins/init.lua
+++ lua/viacheslav/plugins/init.lua
@@ -10,6 +10,19 @@ return {
     'tpope/vim-sleuth',
 
     {
+        'windwp/nvim-ts-autotag',
+        config = function()
+            require('nvim-ts-autotag').setup({
+                opts = {
+                    enable_close = true,         -- Auto close tags
+                    enable_rename = true,        -- Auto rename pairs of tags
+                    enable_close_on_slash = true -- Auto close on trailing </
+                },
+            })
+        end,
+    },
+
+    {
         "RRethy/vim-illuminate",
         event = "VeryLazy",
         config = function()
