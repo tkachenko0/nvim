af59c01729f3757e426105a2aedca65cc5be9b64
viacheslav.tkachenko <t.viacheslav00@gmail.com>
2025-06-02 15:47:20 +0200

    nvim

--- lua/viacheslav/plugins/filetree.lua
+++ lua/viacheslav/plugins/filetree.lua
@@ -10,14 +10,16 @@ return {
     cmd = "Neotree",
     keys = {
       -- { "<leader>e", "<cmd>Neotree filesystem reveal toggle<CR>" },
-      { "<leader>b", "<cmd>Neotree buffers reveal toggle<CR>" },
+      -- { "<leader>b", "<cmd>Neotree buffers reveal toggle<CR>" },
+      -- { '\\', ':Neotree buffers reveal toggle<CR>' }
+      { "\\", "<cmd>Neotree filesystem reveal toggle<CR>" },
     },
     config = function()
       require("neo-tree").setup({
         close_if_last_window = true,
 
         sources = {
-          -- "filesystem",
+          "filesystem",
           "buffers",
           "git_status"
         },
@@ -26,7 +28,7 @@ return {
           winbar = true,
           tabs_layout = "stacked",
           sources = {
-            -- { source = "filesystem", display_name = "󰉓 Files" },
+            { source = "filesystem", display_name = "󰉓 Files" },
             { source = "buffers", display_name = "󰈚 Buffers" },
             { source = "git_status", display_name = "󰊢 Git" },
           },
@@ -48,7 +50,7 @@ return {
         },
 
         window = {
-          position = "right",
+          position = "left",
           mappings = {
             ["<cr>"] = "open",
             ["<Tab>"] = "next_source",
@@ -106,47 +108,48 @@ return {
     end,
   },
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      local function on_attach(bufnr)
-        local api = require("nvim-tree.api")
-        local opts = function(desc)
-          return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-        end
-        api.config.mappings.default_on_attach(bufnr)
-        vim.keymap.set("n", "<Right>", api.node.open.edit, opts("Open Node"))
-        vim.keymap.set("n", "<Left>", api.node.navigate.parent_close, opts("Close Parent Node"))
-        vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
-      end
-      require("nvim-tree").setup({
-        view = {
-          width = 35,
-          side = "left",
-          adaptive_size = true,
-          preserve_window_proportions = true,
-          number = false,
-          relativenumber = false,
-          signcolumn = "yes",
-        },
-        git = {
-          enable = true,
-          ignore = false,
-        },
-        actions = {
-          open_file = {
-            resize_window = true,
-          },
-        },
-        update_focused_file = {
-          enable = true,
-          update_cwd = true,
-        },
-        on_attach = on_attach,
-      })
-    end,
-  },
+  --   {
+  --     "nvim-tree/nvim-tree.lua",
+  --     dependencies = {
+  --       "nvim-tree/nvim-web-devicons",
+  --     },
+  --     config = function()
+  --       local function on_attach(bufnr)
+  --         local api = require("nvim-tree.api")
+  --         local opts = function(desc)
+  --           return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  --         end
+  --         api.config.mappings.default_on_attach(bufnr)
+  --         vim.keymap.set("n", "<Right>", api.node.open.edit, opts("Open Node"))
+  --         vim.keymap.set("n", "<Left>", api.node.navigate.parent_close, opts("Close Parent Node"))
+  --         vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
+  --       end
+  --       require("nvim-tree").setup({
+  --         view = {
+  --           width = 35,
+  --           side = "left",
+  --           adaptive_size = true,
+  --           preserve_window_proportions = true,
+  --           number = false,
+  --           relativenumber = false,
+  --           signcolumn = "yes",
+  --         },
+  --         git = {
+  --           enable = true,
+  --           ignore = false,
+  --         },
+  --         actions = {
+  --           open_file = {
+  --             resize_window = true,
+  --           },
+  --         },
+  --         update_focused_file = {
+  --           enable = true,
+  --           update_cwd = true,
+  --         },
+  --         on_attach = on_attach,
+  --       })
+  --     end,
+  --   },
+
 }
