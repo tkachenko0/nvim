9120fa2e6b5af7f3eed963a32d050b62eb1d37b6
viacheslav.tkachenko <t.viacheslav00@gmail.com>
2025-06-04 00:34:18 +0200

    configs

--- lua/viacheslav/plugins/diffview.lua
+++ lua/viacheslav/plugins/diffview.lua
@@ -17,6 +17,22 @@ return {
         { '<leader>dc', '<cmd>DiffviewClose<cr>',       desc = 'Close Diffview' },
     },
     config = function()
-        require('diffview').setup({})
+        require('diffview').setup({
+            keymaps = {
+                file_panel = {
+                    { "n", "a", require("diffview.actions").toggle_stage_entry, { desc = "Stage / unstage the selected entry." } },
+                },
+            },
+            hooks = {
+                view_opened = function()
+                    -- Hide tabline/bufferline when diffview opens
+                    vim.opt.showtabline = 0
+                end,
+                view_closed = function()
+                    -- Restore tabline/bufferline when diffview closes
+                    vim.opt.showtabline = 2
+                end,
+            },
+        })
     end,
 }
