17812c4a92d2bf9f618a832cb5fc89ca49ad63a3
viacheslav.tkachenko <t.viacheslav00@gmail.com>
2025-06-03 22:45:14 +0200

    depths

--- lua/viacheslav/plugins/formatting.lua
+++ lua/viacheslav/plugins/formatting.lua
@@ -1,7 +1,7 @@
 return {
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
+    cmd = { 'ConformInfo', 'AutoformatDisable', 'AutoformatEnable' },
     keys = {
         {
             "<leader>f",
@@ -13,13 +13,14 @@ return {
     },
     opts = {
         notify_on_error = false,
-        -- format_on_save = function()
-        --     return {
-        --         timeout_ms = 3000,
-        --         lsp_format = 'fallback',
-        --     }
-        -- end,
-        format_on_save = false,
+        format_on_save = function(bufnr)
+            -- You can store disabled buffers in a table if needed for fine-grained control
+            return not vim.g.autoformat_disabled
+                and {
+                    timeout_ms = 3000,
+                    lsp_fallback = true,
+                }
+        end,
         formatters_by_ft = {
             javascript = { "prettier" },
             typescript = { "prettier" },
@@ -38,4 +39,21 @@ return {
             python = { "isort", "black" },
         },
     },
+    config = function(_, opts)
+        require("conform").setup(opts)
+
+        -- Global flag for autoformat
+        vim.g.autoformat_disabled = false
+
+        -- Toggle commands
+        vim.api.nvim_create_user_command("AutoformatDisable", function()
+            vim.g.autoformat_disabled = true
+            print("Autoformat on save: Disabled")
+        end, {})
+
+        vim.api.nvim_create_user_command("AutoformatEnable", function()
+            vim.g.autoformat_disabled = false
+            print("Autoformat on save: Enabled")
+        end, {})
+    end,
 }
