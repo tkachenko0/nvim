ceaebdf17493acc0c54c4530fb3e041e78741649
viacheslav.tkachenko <t.viacheslav00@gmail.com>
2025-06-04 19:59:54 +0200

    test

--- lua/viacheslav/plugins/treesitter.lua
+++ lua/viacheslav/plugins/treesitter.lua
@@ -1,4 +1,5 @@
 return {
+
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
@@ -51,8 +52,10 @@ return {
             })
         end,
     },
+
     {
         "nvim-treesitter/nvim-treesitter-textobjects",
         dependencies = { "nvim-treesitter/nvim-treesitter" },
     },
+
 }
