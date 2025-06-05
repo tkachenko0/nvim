51d445b3c77e51a5e641f74e68726e52ee5c7539
viacheslav.tkachenko <t.viacheslav00@gmail.com>
2025-06-05 13:57:22 +0200

    d

--- lua/viacheslav/plugins/bufferline.lua
+++ lua/viacheslav/plugins/bufferline.lua
@@ -6,7 +6,20 @@ return {
         'nvim-tree/nvim-web-devicons',
     },
     config = function()
+        local get_hex = require('cokeline.hlgroups').get_hl_attr
         require('cokeline').setup({
+            default_hl = {
+                fg = function(buffer)
+                    return buffer.is_focused
+                        and get_hex('Normal', 'fg')
+                        or get_hex('Normal', 'fg')
+                end,
+                bg = function(buffer)
+                    return buffer.is_focused
+                        and get_hex('ColorColumn', 'bg')
+                        or get_hex('TabLine', 'bg')
+                end,
+            },
             sidebar = {
                 filetype = { 'NvimTree', 'neo-tree' },
                 components = {
