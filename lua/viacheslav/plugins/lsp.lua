fb3bce70ef1c2b2c1f6424bdf928f0a91bd40ef7
viacheslav.tkachenko <t.viacheslav00@gmail.com>
2025-06-05 21:19:01 +0200

    fix(checklist): ids

--- lua/viacheslav/plugins/lsp.lua
+++ lua/viacheslav/plugins/lsp.lua
@@ -29,7 +29,6 @@ return {
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
-                "rust_analyzer",
                 "ts_ls",
                 "cssls",
                 "html",
