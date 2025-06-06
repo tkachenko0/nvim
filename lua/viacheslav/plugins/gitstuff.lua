ad2f0bed753201988598a621b8ca7ae6f90b79e8
viacheslav.tkachenko <t.viacheslav00@gmail.com>
2025-06-06 23:19:01 +0200

    b

--- lua/viacheslav/plugins/gitstuff.lua
+++ lua/viacheslav/plugins/gitstuff.lua
@@ -4,7 +4,7 @@ return {
 		"lewis6991/gitsigns.nvim",
 		event = { "BufReadPre", "BufNewFile" },
 		opts = {
-			current_line_blame = false,
+			current_line_blame = true,
 			current_line_blame_opts = {
 				virt_text = true,
 				delay = 0,
