39417e137ac2a7cb7719b408f8edc258fc7fee5b
viacheslav.tkachenko <t.viacheslav00@gmail.com>
2025-06-07 15:55:16 +0200

    a

--- lua/viacheslav/plugins/colorsheme.lua
+++ lua/viacheslav/plugins/colorsheme.lua
@@ -10,6 +10,14 @@ return
                 disable_nvimtree_bg = true,
             })
             vim.cmd("colorscheme vscode")
+
+            -- Git diff colors (Visual Studio-like)
+            vim.cmd([[
+                highlight DiffAdd    guibg=#144212 guifg=NONE gui=NONE
+                highlight DiffDelete guibg=#600000 guifg=NONE gui=NONE
+                highlight DiffChange guibg=#1B2A4A guifg=NONE gui=NONE
+                highlight DiffText   guibg=#265478 gui=bold
+            ]])
         end,
     },
 
