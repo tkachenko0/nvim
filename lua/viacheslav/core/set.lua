6e05d33cd63852f8837aaec3e79cb217a3d1915d
viacheslav.tkachenko <t.viacheslav00@gmail.com>
2025-07-01 20:38:06 +0200

    refactor(formatting): formatted

--- lua/viacheslav/core/set.lua
+++ lua/viacheslav/core/set.lua
@@ -1,5 +1,6 @@
-vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.opt.nu = true
+
 vim.o.mouse = 'a'
 
 -- Highlight the current line
@@ -14,32 +15,27 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
-vim.opt.wrap = false
-vim.opt.swapfile = false
-vim.opt.backup = false
+vim.opt.wrap = false     -- Line wrapping
+vim.opt.swapfile = false -- .swp files creation
+vim.opt.backup = false   -- Disable backup files
 
 -- Search settings
-vim.o.ignorecase = true  -- Ignore case when searching
-vim.o.smartcase = true   -- Override ignorecase if search contains uppercase letters
-vim.opt.hlsearch = true  -- Highlight search results
-vim.opt.incsearch = true -- Show search results as you type
+vim.o.ignorecase = true                 -- Ignore case when searching
+vim.o.smartcase = true                  -- Override ignorecase if search contains uppercase letters
+vim.opt.hlsearch = true                 -- Highlight search results
+vim.opt.incsearch = true                -- Show search results as you type
 
+vim.opt.scrolloff = 12                  -- Keep 12 lines visible above/below the cursor
 
-vim.opt.scrolloff = 12
+vim.opt.isfname:append("@-@")           -- Allow '@' in filenames
 
-vim.opt.signcolumn = "yes" -- For things like git signs, diagnostics, breakpoints, etc.
+vim.opt.clipboard:append("unnamedplus") -- Use the system clipboard for all yanks
 
-vim.opt.isfname:append("@-@")
+vim.opt.fillchars:append({ eob = " " }) -- Eliminate the end-of-buffer characters
 
-vim.opt.clipboard:append("unnamedplus")
+vim.opt.termguicolors = true            -- Enable 24-bit RGB color in the TUI
 
-vim.opt.fillchars:append({ eob = " " })
-
-vim.opt.termguicolors = true
-
-vim.opt.updatetime = 50 -- Enable file system watcher
-
-vim.opt.autoread = true -- Auto-reload files when changed externally
+vim.opt.signcolumn = "yes"              -- Activate sign column from the start for things like diagnostics, gitsigns, etc.
 
 -- Folding
 vim.opt.foldmethod = "expr"
@@ -50,14 +46,21 @@ vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
 vim.opt.fillchars:append({ fold = " " }) -- Clean fold fill character
 
--- Trigger checktime more frequently
+-- Auto-reload files when changed externally
+vim.opt.autoread = true
+vim.opt.updatetime = 50
 vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
     pattern = "*",
     command = "if mode() != 'c' | checktime | endif",
 })
 
 vim.diagnostic.config({
-    virtual_text = { severity = vim.diagnostic.severity.ERROR },
+    -- virtual_text = {
+    --     severity = vim.diagnostic.severity.ERROR
+    -- },
     update_in_insert = true,
-    float = { source = "always", border = "rounded" },
+    float = {
+        source = "always",
+        border = "rounded"
+    },
 })
