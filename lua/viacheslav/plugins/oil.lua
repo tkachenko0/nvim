6366ac510d1017256d8ecbfe65b65cbff28772c3
viacheslav.tkachenko <t.viacheslav00@gmail.com>
2025-07-03 07:29:41 +0200

    oil

--- lua/viacheslav/plugins/oil.lua
+++ lua/viacheslav/plugins/oil.lua
@@ -7,9 +7,11 @@ return {
         require('oil').setup({
             view_options = {
                 show_hidden = true,
-            }
+            },
+            keymaps = {
+                ["<C-c>"] = { "actions.close", mode = "n" },
+            },
         })
         vim.keymap.set("n", "\\", "<CMD>Oil<CR>", { desc = "Open parent directory" })
     end,
-
 }
