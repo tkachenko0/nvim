2c138848571e94f627260bfd086ba4c7e98435d2
viacheslav.tkachenko <t.viacheslav00@gmail.com>
2025-07-07 22:45:57 +0200

    feat(linting): diasabled anoing error

--- lua/viacheslav/plugins/linting.lua
+++ lua/viacheslav/plugins/linting.lua
@@ -3,9 +3,16 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		local lint = require("lint")
+
 		lint.linters_by_ft = {
 			markdown = { "markdownlint" },
 		}
+
+		lint.linters.markdownlint.args = {
+			"--disable", "MD013",
+			"--stdin",
+		}
+
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			callback = function()
 				lint.try_lint()
