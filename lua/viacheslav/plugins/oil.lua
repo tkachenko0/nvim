496dab3ef9cf6b48bb32c3112356dd57e882d95e
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-07-10 11:20:16 +0200

    chore(deps): deps and files

--- lua/viacheslav/plugins/oil.lua
+++ lua/viacheslav/plugins/oil.lua
@@ -5,6 +5,13 @@ return {
     lazy = false,
     config = function()
         require('oil').setup({
+            columns = {
+                -- "icon",
+                -- "permissions",
+                -- "size",
+                -- "mtime",
+
+            },
             view_options = {
                 show_hidden = true,
             },
@@ -13,5 +20,6 @@ return {
             },
         })
         vim.keymap.set("n", "\\", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+        vim.api.nvim_set_hl(0, "OilHidden", { fg = "#6c7086" })
     end,
 }
