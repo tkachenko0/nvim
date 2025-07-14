93b12eabe0f17e1c019f641b5647120cab76ad9f
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-07-14 14:01:11 +0200

    deps

--- lua/viacheslav/plugins/dashboard.lua
+++ lua/viacheslav/plugins/dashboard.lua
@@ -1,42 +1,44 @@
-return {
-    "goolord/alpha-nvim",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-        local alpha = require("alpha")
-        local startify = require("alpha.themes.startify")
+return {}
 
-        startify.section.header.val = {
-            [[            _ _,-'_)_  (""`'-._\ `.     ]],
-            [[           |,' ,-' __)  ,-     /. |     ]],
-            [[           |     -'  _)/         `\     ]],
-            [[          ,'       ,-'_,`           :   ]],
-            [[       ,-'       ,(,-(              :   ]],
-            [[     ,'       ,-' ,    _            ;   ]],
-            [[    /        ,-._/`---'            /    ]],
-            [[   /        (____)(----. )       ,'     ]],
-            [[  /         (      `.__,     /\ /,      ]],
-            [[ :           ;-.___         /__\/|      ]],
-            [[|         ,'      `--.      -,\ |       ]],
-            [[:        /            \    .__/         ]],
-            [[\      (__            \    |_           ]],
-            [[  \       ,`-,         /   _|,\         ]],
-            [[   \    ,'   `-.     ,'_,-'    \        ]],
-            [[  (_\,-'    ,'\")--,'-'       __\       ]],
-            [[  \       /  // ,'|      ,--'  `-.      ]],
-            [[   `-.    `-/ \'  |   _,'         `.    ]],
-            [[       `-._ /      `--'/             \  ]],
-            [[          ,'           |              \ ]],
-            [[        /             |               \ ]],
-            [[     ,-'              |               / ]],
-            [[    /                 |             -'  ]],
-        }
-
-        startify.section.top_buttons.val = {}
-        startify.section.mru.val = { { type = "padding", val = 0 } }
-        startify.section.bottom_buttons.val = {}
-
-        alpha.setup(startify.opts)
-    end,
-}
+-- return {
+--     "goolord/alpha-nvim",
+--     dependencies = {
+--         "nvim-tree/nvim-web-devicons",
+--     },
+--     config = function()
+--         local alpha = require("alpha")
+--         local startify = require("alpha.themes.startify")
+--
+--         startify.section.header.val = {
+--             [[            _ _,-'_)_  (""`'-._\ `.     ]],
+--             [[           |,' ,-' __)  ,-     /. |     ]],
+--             [[           |     -'  _)/         `\     ]],
+--             [[          ,'       ,-'_,`           :   ]],
+--             [[       ,-'       ,(,-(              :   ]],
+--             [[     ,'       ,-' ,    _            ;   ]],
+--             [[    /        ,-._/`---'            /    ]],
+--             [[   /        (____)(----. )       ,'     ]],
+--             [[  /         (      `.__,     /\ /,      ]],
+--             [[ :           ;-.___         /__\/|      ]],
+--             [[|         ,'      `--.      -,\ |       ]],
+--             [[:        /            \    .__/         ]],
+--             [[\      (__            \    |_           ]],
+--             [[  \       ,`-,         /   _|,\         ]],
+--             [[   \    ,'   `-.     ,'_,-'    \        ]],
+--             [[  (_\,-'    ,'\")--,'-'       __\       ]],
+--             [[  \       /  // ,'|      ,--'  `-.      ]],
+--             [[   `-.    `-/ \'  |   _,'         `.    ]],
+--             [[       `-._ /      `--'/             \  ]],
+--             [[          ,'           |              \ ]],
+--             [[        /             |               \ ]],
+--             [[     ,-'              |               / ]],
+--             [[    /                 |             -'  ]],
+--         }
+--
+--         startify.section.top_buttons.val = {}
+--         startify.section.mru.val = { { type = "padding", val = 0 } }
+--         startify.section.bottom_buttons.val = {}
+--
+--         alpha.setup(startify.opts)
+--     end,
+-- }
