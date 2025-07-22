195b3f18e599806b5c08fb763cb32037b226c205
viacheslav.tkachenko <t.viacheslav00@gmail.com>
2025-07-23 00:46:43 +0200

    style(select): styled select

--- lua/viacheslav/plugins/lualine.lua
+++ lua/viacheslav/plugins/lualine.lua
@@ -3,6 +3,10 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
         require("lualine").setup {
+            options = {
+                section_separators = { left = '', right = '' },
+                component_separators = { left = '', right = '' },
+            },
             sections = {
                 lualine_a = {
                 },
