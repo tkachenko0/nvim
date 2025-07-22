1a0e0997a069c28eac80b0d14d7cfb7a1917420e
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-07-22 15:14:47 +0200

    chore(format): formatted

--- lua/viacheslav/plugins/dashboard.lua
+++ lua/viacheslav/plugins/dashboard.lua
@@ -1,44 +1,42 @@
-return {}
+return {
+    "goolord/alpha-nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        local alpha = require("alpha")
+        local startify = require("alpha.themes.startify")
 
--- return {
---     "goolord/alpha-nvim",
---     dependencies = {
---         "nvim-tree/nvim-web-devicons",
---     },
---     config = function()
---         local alpha = require("alpha")
---         local startify = require("alpha.themes.startify")
---
---         startify.section.header.val = {
---             [[            _ _,-'_)_  (""`'-._\ `.     ]],
---             [[           |,' ,-' __)  ,-     /. |     ]],
---             [[           |     -'  _)/         `\     ]],
---             [[          ,'       ,-'_,`           :   ]],
---             [[       ,-'       ,(,-(              :   ]],
---             [[     ,'       ,-' ,    _            ;   ]],
---             [[    /        ,-._/`---'            /    ]],
---             [[   /        (____)(----. )       ,'     ]],
---             [[  /         (      `.__,     /\ /,      ]],
---             [[ :           ;-.___         /__\/|      ]],
---             [[|         ,'      `--.      -,\ |       ]],
---             [[:        /            \    .__/         ]],
---             [[\      (__            \    |_           ]],
---             [[  \       ,`-,         /   _|,\         ]],
---             [[   \    ,'   `-.     ,'_,-'    \        ]],
---             [[  (_\,-'    ,'\")--,'-'       __\       ]],
---             [[  \       /  // ,'|      ,--'  `-.      ]],
---             [[   `-.    `-/ \'  |   _,'         `.    ]],
---             [[       `-._ /      `--'/             \  ]],
---             [[          ,'           |              \ ]],
---             [[        /             |               \ ]],
---             [[     ,-'              |               / ]],
---             [[    /                 |             -'  ]],
---         }
---
---         startify.section.top_buttons.val = {}
---         startify.section.mru.val = { { type = "padding", val = 0 } }
---         startify.section.bottom_buttons.val = {}
---
---         alpha.setup(startify.opts)
---     end,
--- }
+        startify.section.header.val = {
+            [[            _ _,-'_)_  (""`'-._\ `.     ]],
+            [[           |,' ,-' __)  ,-     /. |     ]],
+            [[           |     -'  _)/         `\     ]],
+            [[          ,'       ,-'_,`           :   ]],
+            [[       ,-'       ,(,-(              :   ]],
+            [[     ,'       ,-' ,    _            ;   ]],
+            [[    /        ,-._/`---'            /    ]],
+            [[   /        (____)(----. )       ,'     ]],
+            [[  /         (      `.__,     /\ /,      ]],
+            [[ :           ;-.___         /__\/|      ]],
+            [[|         ,'      `--.      -,\ |       ]],
+            [[:        /            \    .__/         ]],
+            [[\      (__            \    |_           ]],
+            [[  \       ,`-,         /   _|,\         ]],
+            [[   \    ,'   `-.     ,'_,-'    \        ]],
+            [[  (_\,-'    ,'\")--,'-'       __\       ]],
+            [[  \       /  // ,'|      ,--'  `-.      ]],
+            [[   `-.    `-/ \'  |   _,'         `.    ]],
+            [[       `-._ /      `--'/             \  ]],
+            [[          ,'           |              \ ]],
+            [[        /             |               \ ]],
+            [[     ,-'              |               / ]],
+            [[    /                 |             -'  ]],
+        }
+
+        startify.section.top_buttons.val = {}
+        startify.section.mru.val = { { type = "padding", val = 0 } }
+        startify.section.bottom_buttons.val = {}
+
+        alpha.setup(startify.opts)
+    end,
+}
