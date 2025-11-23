0c40a3fe8864b5079af0a4eee46a42d132512a41
viacheslav.tkachenko <t.viacheslav00@gmail.com>
2025-11-23 17:43:40 +0100

    fix(db): fixed cmp integration with db plugin

--- lua/viacheslav/plugins/database.lua
+++ lua/viacheslav/plugins/database.lua
@@ -1,36 +1,31 @@
 return {
-
-    {
-        {
-            'tpope/vim-dadbod',
-            dependencies = {
-                'kristijanhusak/vim-dadbod-ui',
-                'kristijanhusak/vim-dadbod-completion',
-            },
-            config = function()
-                vim.g.db_ui_use_nerd_fonts = 1
-                vim.g.db_ui_show_database_icon = 1
-                vim.g.db_ui_force_echo_messages = 1
-                vim.g.db_ui_execute_on_save = 0 -- Disable auto-execution on save
-                vim.g.db_ui_save_location = '~/dev/databases'
-
-                vim.api.nvim_create_autocmd('FileType', {
-                    pattern = { 'sql', 'mysql', 'plsql' },
-                    callback = function()
-                        require('cmp').setup.buffer({
-                            sources = {
-                                { name = 'vim-dadbod-completion' },
-                                { name = 'buffer' },
-                            }
-                        })
-                    end,
-                })
-
-                vim.keymap.set('n', '<leader>db', '<cmd>DBUIToggle<CR>', { desc = 'Toggle DBUI' })
-                vim.keymap.set('n', '<leader>r', ':%DB<CR>', { desc = 'Run SQL file' })
-                vim.keymap.set('v', '<leader>r', ':DB<CR>', { desc = 'Run SQL selection' })
+    'tpope/vim-dadbod',
+    dependencies = {
+        'kristijanhusak/vim-dadbod-ui',
+        'kristijanhusak/vim-dadbod-completion',
+    },
+    cmd = { 'DBUI', 'DBUIToggle', 'DBUIAddConnection', 'DB' },
+    keys = {
+        { '<leader>db', '<cmd>DBUIToggle<CR>', desc = 'Toggle DBUI' },
+        { '<leader>r', ':%DB<CR>', desc = 'Run SQL file', mode = 'n' },
+        { '<leader>r', ':DB<CR>', desc = 'Run SQL selection', mode = 'v' },
+    },
+    init = function()
+        vim.g.db_ui_use_nerd_fonts = 1
+        vim.g.db_ui_show_database_icon = 1
+        vim.g.db_ui_force_echo_messages = 1
+        vim.g.db_ui_execute_on_save = 0
+        vim.g.db_ui_save_location = '~/dev/databases'
+    end,
+    config = function()
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = { 'sql', 'mysql', 'plsql' },
+            callback = function()
+                local cmp = require('cmp')
+                local sources = cmp.get_config().sources
+                table.insert(sources, { name = 'vim-dadbod-completion' })
+                cmp.setup.buffer({ sources = sources })
             end,
-        }
-    }
-
+        })
+    end,
 }
