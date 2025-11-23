5856c32f4c09a5b9777984fabb7d7baf78dbfede
viacheslav.tkachenko <t.viacheslav00@gmail.com>
2025-11-23 01:30:30 +0100

    feat(file): xml

--- lua/viacheslav/plugins/database.lua
+++ lua/viacheslav/plugins/database.lua
@@ -8,14 +8,12 @@ return {
                 'kristijanhusak/vim-dadbod-completion',
             },
             config = function()
-                -- Global variables for dadbod-ui
                 vim.g.db_ui_use_nerd_fonts = 1
                 vim.g.db_ui_show_database_icon = 1
                 vim.g.db_ui_force_echo_messages = 1
                 vim.g.db_ui_execute_on_save = 0 -- Disable auto-execution on save
                 vim.g.db_ui_save_location = '~/dev/databases'
 
-                -- Auto-completion setup
                 vim.api.nvim_create_autocmd('FileType', {
                     pattern = { 'sql', 'mysql', 'plsql' },
                     callback = function()
@@ -28,13 +26,9 @@ return {
                     end,
                 })
 
-                -- Keymaps
                 vim.keymap.set('n', '<leader>db', '<cmd>DBUIToggle<CR>', { desc = 'Toggle DBUI' })
-                vim.keymap.set('n', '<leader>df', '<cmd>DBUIFindBuffer<CR>', { desc = 'Find DB buffer' })
-                vim.keymap.set('n', '<leader>dr', '<cmd>DBUIRenameBuffer<CR>', { desc = 'Rename DB buffer' })
-                vim.keymap.set('n', '<leader>dq', '<cmd>DBUILastQueryInfo<CR>', { desc = 'Last query info' })
-                vim.keymap.set('n', '<leader>rr', ':%DB<CR>', { desc = 'Run SQL file' })
-                vim.keymap.set('v', '<leader>rr', ':DB<CR>', { desc = 'Run SQL selection' })
+                vim.keymap.set('n', '<leader>r', ':%DB<CR>', { desc = 'Run SQL file' })
+                vim.keymap.set('v', '<leader>r', ':DB<CR>', { desc = 'Run SQL selection' })
             end,
         }
     }
