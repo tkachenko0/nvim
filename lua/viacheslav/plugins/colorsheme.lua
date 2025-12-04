7a9b341f5bf52ff50c825b8608872bef6b651bd7
viacheslav.tkachenko <t.viacheslav00@gmail.com>
2025-12-04 20:19:03 +0100

    feat(theme): theme switcher

--- lua/viacheslav/plugins/colorsheme.lua
+++ lua/viacheslav/plugins/colorsheme.lua
@@ -12,3 +12,53 @@ return {
         vim.api.nvim_set_hl(0, "DiffText", { bg = "#265478", bold = true })
     end,
 }
+
+
+
+-- return {
+--     { "folke/tokyonight.nvim" },
+--     { "catppuccin/nvim", name = "catppuccin" },
+--     { "rebelot/kanagawa.nvim" },
+--     { "EdenEast/nightfox.nvim" },
+--     { "rose-pine/neovim", name = "rose-pine" },
+--     { "Mofiqul/vscode.nvim" },
+--     { "navarasu/onedark.nvim" },
+--     { "projekt0n/github-nvim-theme" },
+--     { "sainnhe/gruvbox-material" },
+--     { "sainnhe/everforest" },
+--     { "sainnhe/sonokai" },
+--     { "Mofiqul/dracula.nvim" },
+--     { "marko-cerovac/material.nvim" },
+--     { "bluz71/vim-nightfly-colors", name = "nightfly" },
+--     { "bluz71/vim-moonfly-colors", name = "moonfly" },
+--     { "shaunsingh/nord.nvim" },
+--     { "olimorris/onedarkpro.nvim" },
+--     { "AlexvZyl/nordic.nvim" },
+--     { "Shatur/neovim-ayu" },
+--     { "Everblush/nvim", name = "everblush" },
+--     { "kvrohit/mellow.nvim" },
+--     { "Yazeed1s/minimal.nvim" },
+--     {
+--         "rktjmp/lush.nvim",
+--         config = function()
+--             local themes = {
+--                 "tokyonight", "catppuccin", "kanagawa", "nightfox", "rose-pine",
+--                 "vscode", "onedark", "github_dark", "gruvbox-material", "everforest",
+--                 "sonokai", "dracula", "material", "nightfly", "moonfly",
+--                 "nord", "onedarkpro", "nordic", "ayu-dark", "everblush",
+--                 "mellow", "minimal"
+--             }
+--
+--             vim.cmd("colorscheme vscode")
+--
+--             vim.keymap.set("n", "<leader>th", function()
+--                 vim.ui.select(themes, { prompt = "Scegli tema:" }, function(choice)
+--                     if choice then
+--                         pcall(vim.cmd, "colorscheme " .. choice)
+--                     end
+--                 end)
+--             end, { desc = "Cambia tema" })
+--         end,
+--     },
+-- }
+--
