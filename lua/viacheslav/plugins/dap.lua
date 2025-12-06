2e9467e6c6d75b79b39720381e88dae28af968d3
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-12-06 21:39:05 +0100

    feat(debugger): commented for now

--- lua/viacheslav/plugins/dap.lua
+++ lua/viacheslav/plugins/dap.lua
@@ -1,80 +1,82 @@
-return {
-  {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-      "rcarriga/nvim-dap-ui",
-      "nvim-neotest/nvim-nio",
-      "mxsdev/nvim-dap-vscode-js",
-    },
-    config = function()
-      local dap = require("dap")
-      local dapui = require("dapui")
+return {}
 
-      dapui.setup()
-
-      -- Auto-open/close UI
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
-
-      -- Python
-      dap.adapters.python = {
-        type = "executable",
-        command = "python",
-        args = { "-m", "debugpy.adapter" },
-      }
-      dap.configurations.python = {
-        {
-          type = "python",
-          request = "launch",
-          name = "Launch file",
-          program = "${file}",
-        },
-      }
-
-      -- TypeScript/JavaScript
-      require("dap-vscode-js").setup({
-        debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
-        adapters = { "pwa-node", "pwa-chrome", "node-terminal" },
-      })
-
-      for _, language in ipairs({ "typescript", "javascript" }) do
-        dap.configurations[language] = {
-          {
-            type = "pwa-node",
-            request = "launch",
-            name = "Launch file",
-            program = "${file}",
-            cwd = "${workspaceFolder}",
-          },
-          {
-            type = "pwa-node",
-            request = "attach",
-            name = "Attach",
-            processId = require("dap.utils").pick_process,
-            cwd = "${workspaceFolder}",
-          },
-        }
-      end
-
-      -- Keymaps
-      vim.keymap.set("n", "<leader>dB", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
-      vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue" })
-      vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "Step Into" })
-      vim.keymap.set("n", "<leader>do", dap.step_over, { desc = "Step Over" })
-      vim.keymap.set("n", "<leader>dO", dap.step_out, { desc = "Step Out" })
-      vim.keymap.set("n", "<leader>dt", dap.terminate, { desc = "Terminate" })
-      vim.keymap.set("n", "<leader>du", dapui.toggle, { desc = "Toggle UI" })
-    end,
-  },
-  {
-    "microsoft/vscode-js-debug",
-    build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
-  },
-}
+-- return {
+--   {
+--     "mfussenegger/nvim-dap",
+--     dependencies = {
+--       "rcarriga/nvim-dap-ui",
+--       "nvim-neotest/nvim-nio",
+--       "mxsdev/nvim-dap-vscode-js",
+--     },
+--     config = function()
+--       local dap = require("dap")
+--       local dapui = require("dapui")
+--
+--       dapui.setup()
+--
+--       -- Auto-open/close UI
+--       dap.listeners.after.event_initialized["dapui_config"] = function()
+--         dapui.open()
+--       end
+--       dap.listeners.before.event_terminated["dapui_config"] = function()
+--         dapui.close()
+--       end
+--       dap.listeners.before.event_exited["dapui_config"] = function()
+--         dapui.close()
+--       end
+--
+--       -- Python
+--       dap.adapters.python = {
+--         type = "executable",
+--         command = "python",
+--         args = { "-m", "debugpy.adapter" },
+--       }
+--       dap.configurations.python = {
+--         {
+--           type = "python",
+--           request = "launch",
+--           name = "Launch file",
+--           program = "${file}",
+--         },
+--       }
+--
+--       -- TypeScript/JavaScript
+--       require("dap-vscode-js").setup({
+--         debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
+--         adapters = { "pwa-node", "pwa-chrome", "node-terminal" },
+--       })
+--
+--       for _, language in ipairs({ "typescript", "javascript" }) do
+--         dap.configurations[language] = {
+--           {
+--             type = "pwa-node",
+--             request = "launch",
+--             name = "Launch file",
+--             program = "${file}",
+--             cwd = "${workspaceFolder}",
+--           },
+--           {
+--             type = "pwa-node",
+--             request = "attach",
+--             name = "Attach",
+--             processId = require("dap.utils").pick_process,
+--             cwd = "${workspaceFolder}",
+--           },
+--         }
+--       end
+--
+--       -- Keymaps
+--       vim.keymap.set("n", "<leader>dB", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
+--       vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue" })
+--       vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "Step Into" })
+--       vim.keymap.set("n", "<leader>do", dap.step_over, { desc = "Step Over" })
+--       vim.keymap.set("n", "<leader>dO", dap.step_out, { desc = "Step Out" })
+--       vim.keymap.set("n", "<leader>dt", dap.terminate, { desc = "Terminate" })
+--       vim.keymap.set("n", "<leader>du", dapui.toggle, { desc = "Toggle UI" })
+--     end,
+--   },
+--   {
+--     "microsoft/vscode-js-debug",
+--     build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
+--   },
+-- }
