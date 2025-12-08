c9d78373307e5b9a29b522863e2d3e9411735952
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-12-09 00:57:41 +0100

    feat(debugger): added dap

--- lua/viacheslav/plugins/bufferline.lua
+++ lua/viacheslav/plugins/bufferline.lua
@@ -1,23 +1,25 @@
-return {
-    'willothy/nvim-cokeline',
-    enabled = false,
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        'nvim-tree/nvim-web-devicons',
-    },
-    config = function()
-        require('cokeline').setup({
-            show_if_buffers_are_at_least = 2,
-            default_hl = {
-                fg = function(buffer)
-                    return buffer.is_focused and '#cccccc' or '#888888'
-                end,
-                bg = function(buffer)
-                    return buffer.is_focused and '#3c3836' or '#1d2021'
-                end,
-            },
-        })
-        vim.keymap.set('n', '<A-Left>', '<Plug>(cokeline-focus-prev)')
-        vim.keymap.set('n', '<A-Right>', '<Plug>(cokeline-focus-next)')
-    end,
-}
+return {}
+
+-- return {
+--     'willothy/nvim-cokeline',
+--     enabled = false,
+--     dependencies = {
+--         'nvim-lua/plenary.nvim',
+--         'nvim-tree/nvim-web-devicons',
+--     },
+--     config = function()
+--         require('cokeline').setup({
+--             show_if_buffers_are_at_least = 2,
+--             default_hl = {
+--                 fg = function(buffer)
+--                     return buffer.is_focused and '#cccccc' or '#888888'
+--                 end,
+--                 bg = function(buffer)
+--                     return buffer.is_focused and '#3c3836' or '#1d2021'
+--                 end,
+--             },
+--         })
+--         vim.keymap.set('n', '<A-Left>', '<Plug>(cokeline-focus-prev)')
+--         vim.keymap.set('n', '<A-Right>', '<Plug>(cokeline-focus-next)')
+--     end,
+-- }
--- lua/viacheslav/plugins/colorsheme.lua
+++ lua/viacheslav/plugins/colorsheme.lua
@@ -1,64 +1,21 @@
+-- return {
+--     "Mofiqul/vscode.nvim",
+--     config = function()
+--         require("vscode").setup({
+--             transparent = true,
+--         })
+--         vim.cmd("colorscheme vscode")
+--         -- Git diff colors (Visual Studio-like)
+--         vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#005a00", fg = "NONE" })
+--         vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#5f0000", fg = "NONE" })
+--         vim.api.nvim_set_hl(0, "DiffChange", { bg = "#1B2A4A", fg = "NONE" })
+--         vim.api.nvim_set_hl(0, "DiffText", { bg = "#265478", bold = true })
+--     end,
+-- }
+
 return {
-    "Mofiqul/vscode.nvim",
+    "folke/tokyonight.nvim",
     config = function()
-        require("vscode").setup({
-            transparent = true,
-        })
-        vim.cmd("colorscheme vscode")
-        -- Git diff colors (Visual Studio-like)
-        vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#005a00", fg = "NONE" })
-        vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#5f0000", fg = "NONE" })
-        vim.api.nvim_set_hl(0, "DiffChange", { bg = "#1B2A4A", fg = "NONE" })
-        vim.api.nvim_set_hl(0, "DiffText", { bg = "#265478", bold = true })
+        vim.cmd("colorscheme tokyonight-night")
     end,
 }
-
-
-
--- return {
---     { "folke/tokyonight.nvim" },
---     { "catppuccin/nvim", name = "catppuccin" },
---     { "rebelot/kanagawa.nvim" },
---     { "EdenEast/nightfox.nvim" },
---     { "rose-pine/neovim", name = "rose-pine" },
---     { "Mofiqul/vscode.nvim" },
---     { "navarasu/onedark.nvim" },
---     { "projekt0n/github-nvim-theme" },
---     { "sainnhe/gruvbox-material" },
---     { "sainnhe/everforest" },
---     { "sainnhe/sonokai" },
---     { "Mofiqul/dracula.nvim" },
---     { "marko-cerovac/material.nvim" },
---     { "bluz71/vim-nightfly-colors", name = "nightfly" },
---     { "bluz71/vim-moonfly-colors", name = "moonfly" },
---     { "shaunsingh/nord.nvim" },
---     { "olimorris/onedarkpro.nvim" },
---     { "AlexvZyl/nordic.nvim" },
---     { "Shatur/neovim-ayu" },
---     { "Everblush/nvim", name = "everblush" },
---     { "kvrohit/mellow.nvim" },
---     { "Yazeed1s/minimal.nvim" },
---     {
---         "rktjmp/lush.nvim",
---         config = function()
---             local themes = {
---                 "tokyonight", "catppuccin", "kanagawa", "nightfox", "rose-pine",
---                 "vscode", "onedark", "github_dark", "gruvbox-material", "everforest",
---                 "sonokai", "dracula", "material", "nightfly", "moonfly",
---                 "nord", "onedarkpro", "nordic", "ayu-dark", "everblush",
---                 "mellow", "minimal"
---             }
---
---             vim.cmd("colorscheme vscode")
---
---             vim.keymap.set("n", "<leader>th", function()
---                 vim.ui.select(themes, { prompt = "Scegli tema:" }, function(choice)
---                     if choice then
---                         pcall(vim.cmd, "colorscheme " .. choice)
---                     end
---                 end)
---             end, { desc = "Cambia tema" })
---         end,
---     },
--- }
---
