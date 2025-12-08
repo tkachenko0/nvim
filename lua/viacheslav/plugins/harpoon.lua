b3007dd4112a699add500b0dd63f1891bc977247
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-12-09 00:24:11 +0100

    feat(harpoon): added harpoon

--- lua/viacheslav/plugins/harpoon.lua
+++ lua/viacheslav/plugins/harpoon.lua
@@ -4,7 +4,16 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         local harpoon = require("harpoon")
-        harpoon:setup()
+        harpoon:setup({
+            settings = {
+                save_on_toggle = true,
+                sync_on_ui_close = true,
+            }
+        })
+
+        local harpoon_extensions = require("harpoon.extensions")
+        harpoon:extend(harpoon_extensions.builtins.highlight_current_file())
+
         vim.keymap.set("n", "<leader>A", function() harpoon:list():add() end)
         vim.keymap.set("n", "<A-a>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
     end,
