ff54c45fbcc59756ae0729db941f86bc13ab36f9
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-12-09 02:23:42 +0100

    feat(harpoon): added harpoon

--- lua/viacheslav/core/init.lua
+++ lua/viacheslav/core/init.lua
@@ -15,7 +15,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
     callback = function(e)
         local opts = { buffer = e.buf }
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        -- Other keymaps (shared across all LSPs)
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover { border = "single" } end, opts)
         vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
         vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
