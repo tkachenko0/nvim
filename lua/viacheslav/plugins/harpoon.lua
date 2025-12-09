e064d13887e02338bae1c9b497bbf8a47b423d22
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-12-09 02:52:43 +0100

    automated dev commit

--- lua/viacheslav/plugins/harpoon.lua
+++ lua/viacheslav/plugins/harpoon.lua
@@ -6,15 +6,18 @@ return {
         local harpoon = require("harpoon")
         harpoon:setup({
             settings = {
-                -- save_on_toggle = true,
+                save_on_toggle = true,
                 sync_on_ui_close = true,
             },
-            display = function(list_item)
-                local path = list_item.value
-                local filename = vim.fn.fnamemodify(path, ":t") -- file.ext
-                local dirname = vim.fn.fnamemodify(path, ":p:h:t") -- last directory
-                return string.format("%s / %s", dirname, filename)
-            end,
+            default = {
+                display = function(list_item)
+                    local path = list_item.value
+                    local parent = vim.fn.fnamemodify(path, ":h:t")
+                    local filename = vim.fn.fnamemodify(path, ":t")
+                    if parent == "." then return filename end
+                    return parent .. "/" .. filename
+                end,
+            },
         })
 
         local harpoon_extensions = require("harpoon.extensions")
