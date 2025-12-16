6b802688adcdc08beba9ad53a98f8b4f54de34fa
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-12-16 20:55:53 +0100

    automated dev commit

--- lua/viacheslav/core/init.lua
+++ lua/viacheslav/core/init.lua
@@ -1,38 +1,3 @@
 require("viacheslav.core.set")
 require("viacheslav.core.remap")
-
-vim.api.nvim_create_autocmd('TextYankPost', {
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 200,
-        })
-    end,
-})
-
-vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(e)
-        local opts = { buffer = e.buf }
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover { border = "single" } end, opts)
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-        vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        vim.keymap.set("n", "[D", vim.diagnostic.goto_prev, opts)
-        vim.keymap.set("n", "]D", vim.diagnostic.goto_next, opts)
-        vim.keymap.set("n", "[d", function()
-            vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })
-        end, opts)
-        vim.keymap.set("n", "]d", function()
-            vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })
-        end, opts)
-    end,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "yaml",
-    callback = function()
-        vim.opt_local.cursorcolumn = true
-    end,
-})
+require("viacheslav.core.autocmds")
