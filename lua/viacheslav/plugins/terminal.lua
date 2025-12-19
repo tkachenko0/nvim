f741f6be7d26c1357094ae32639dc9223505810c
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-12-19 13:37:03 +0100

    automated dev commit

--- lua/viacheslav/plugins/terminal.lua
+++ lua/viacheslav/plugins/terminal.lua
@@ -2,7 +2,7 @@ return {
     "akinsho/toggleterm.nvim",
     config = function()
         require("toggleterm").setup({
-            open_mapping = [[<leader>t]],
+            open_mapping = [[<C-\>]],
             direction = "float",
             float_opts = {
                 border = "rounded"
