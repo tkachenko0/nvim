9cccba09ef1684ac131b41425c517a03cb81f297
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-12-23 22:00:41 +0100

    automated dev commit

--- lua/viacheslav/plugins/harpoon.lua
+++ lua/viacheslav/plugins/harpoon.lua
@@ -33,7 +33,7 @@ return {
         --     end,
         -- })
 
-        vim.keymap.set("n", "<leader>A", function() harpoon:list():add() end)
+        vim.keymap.set("n", "<leader><leader>", function() harpoon:list():add() end)
         vim.keymap.set("n", "<A-a>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
     end,
 }
