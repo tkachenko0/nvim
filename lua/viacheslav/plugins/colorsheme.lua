e2cc7166bc4b58515c6d2b0953ad6706ccd107f7
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-12-29 23:08:15 +0100

    automated dev commit

--- lua/viacheslav/plugins/colorsheme.lua
+++ lua/viacheslav/plugins/colorsheme.lua
@@ -1,22 +1,11 @@
--- return {
---     "Mofiqul/vscode.nvim",
---     config = function()
---         require("vscode").setup({
---             transparent = true,
---         })
---         vim.cmd("colorscheme vscode")
---         -- Git diff colors (Visual Studio-like)
---         vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#005a00", fg = "NONE" })
---         vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#5f0000", fg = "NONE" })
---         vim.api.nvim_set_hl(0, "DiffChange", { bg = "#1B2A4A", fg = "NONE" })
---         vim.api.nvim_set_hl(0, "DiffText", { bg = "#265478", bold = true })
---     end,
--- }
-
 return {
-    "folke/tokyonight.nvim",
+    "Mofiqul/vscode.nvim",
     config = function()
-        vim.cmd("colorscheme tokyonight-night")
+        require("vscode").setup({
+            transparent = true,
+        })
+        vim.cmd("colorscheme vscode")
+        -- Git diff colors (Visual Studio-like)
         vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#005a00", fg = "NONE" })
         vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#5f0000", fg = "NONE" })
         vim.api.nvim_set_hl(0, "DiffChange", { bg = "#1B2A4A", fg = "NONE" })
