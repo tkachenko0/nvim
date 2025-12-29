13b79d40933e3ba958d29a46d3227a580e63466e
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-12-29 22:57:17 +0100

    automated dev commit

--- lua/viacheslav/plugins/dashboard.lua
+++ lua/viacheslav/plugins/dashboard.lua
@@ -3,35 +3,10 @@ return {
     dependencies = {
         "nvim-tree/nvim-web-devicons",
     },
-    enabled = false,
+    enabled = true,
     config = function()
         local alpha = require("alpha")
         local startify = require("alpha.themes.startify")
-        startify.section.header.val = {
-            [[            _ _,-'_)_  (""`'-._\ `.     ]],
-            [[           |,' ,-' __)  ,-     /. |     ]],
-            [[           |     -'  _)/         `\     ]],
-            [[          ,'       ,-'_,`           :   ]],
-            [[       ,-'       ,(,-(              :   ]],
-            [[     ,'       ,-' ,    _            ;   ]],
-            [[    /        ,-._/`---'            /    ]],
-            [[   /        (____)(----. )       ,'     ]],
-            [[  /         (      `.__,     /\ /,      ]],
-            [[ :           ;-.___         /__\/|      ]],
-            [[|         ,'      `--.      -,\ |       ]],
-            [[:        /            \    .__/         ]],
-            [[\      (__            \    |_           ]],
-            [[  \       ,`-,         /   _|,\         ]],
-            [[   \    ,'   `-.     ,'_,-'    \        ]],
-            [[  (_\,-'    ,'\")--,'-'       __\       ]],
-            [[  \       /  // ,'|      ,--'  `-.      ]],
-            [[   `-.    `-/ \'  |   _,'         `.    ]],
-            [[       `-._ /      `--'/             \  ]],
-            [[          ,'           |              \ ]],
-            [[        /             |               \ ]],
-            [[     ,-'              |               / ]],
-            [[    /                 |             -'  ]],
-        }
         startify.section.top_buttons.val = {}
         startify.section.mru.val = { { type = "padding", val = 0 } }
         startify.section.bottom_buttons.val = {}
