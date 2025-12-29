0ad4185ab4484f20a9930a90dcc1e347ec532bd1
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-12-30 00:11:21 +0100

    automated commit
    automated commit
    
    automated commit
    
    automated commit
    
    automated commit
    
    automated commit

--- lua/viacheslav/core/remap.lua
+++ lua/viacheslav/core/remap.lua
@@ -11,11 +11,11 @@ vim.keymap.set({ 'n', 'v' }, 'E', '$', { desc = 'Go to end of line' })
 -- Custom scripts
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
--- Move between windows (Will learn hjkl later)
-vim.keymap.set('n', '<C-Left>', '<C-w>h', { desc = 'Move to left split' })
-vim.keymap.set('n', '<C-Down>', '<C-w>j', { desc = 'Move to below split' })
-vim.keymap.set('n', '<C-Up>', '<C-w>k', { desc = 'Move to above split' })
-vim.keymap.set('n', '<C-RIght>', '<C-w>l', { desc = 'Move to right split' })
+-- Move between windows
+vim.keymap.set('n', '<C-Left>', '<C-w>h')
+vim.keymap.set('n', '<C-Down>', '<C-w>j')
+vim.keymap.set('n', '<C-Up>', '<C-w>k')
+vim.keymap.set('n', '<C-RIght>', '<C-w>l')
 
 -- Quickfix navigation
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
@@ -24,11 +24,11 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 -- Clear highlights for search
 vim.keymap.set("n", "<esc>", "<cmd>nohlsearch<cr>", { desc = "Clear highlights" })
 
--- Move lines up and down (Will learn hjkl later)
-vim.keymap.set('v', '<A-Down>', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
-vim.keymap.set('v', '<A-Up>', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
-vim.keymap.set('n', '<A-Down>', ':m .+1<CR>==', { desc = 'Move line down' })
-vim.keymap.set('n', '<A-Up>', ':m .-2<CR>==', { desc = 'Move line up' })
+-- Move lines up and down
+vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv")
+vim.keymap.set('n', '<A-j>', ':m .+1<CR>==')
+vim.keymap.set('n', '<A-k>', ':m .-2<CR>==')
 
 -- Vertical scroll and center
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -39,9 +39,8 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set('v', '<leader>s', '"hy:%s/<C-r>h/<C-r>h/g<Left><Left>')
 
 -- Not copy when deleting
-vim.keymap.set("n", "x", '"_x')
-vim.keymap.set("n", "c", '"_c')
-vim.keymap.set("n", "S", '"_S')
+vim.keymap.set({ "n", "v" }, "S", '"_S')
+vim.keymap.set({ "n", "v" }, "x", '"_x')
 vim.keymap.set({ "n", "v" }, "s", '"_s')
 vim.keymap.set({ "n", "v" }, "d", '"_d')
 
@@ -61,7 +60,7 @@ vim.keymap.set('n', '<', '<<')
 -- Buffers
 vim.keymap.set("n", "<C-c>", "<cmd>%y+<CR>", { desc = "Copy the whole file" })
 vim.keymap.set("n", "<C-a>", "gg<S-v>G", { desc = "Select all" })
-vim.keymap.set('n', '<Tab>', '<C-6>', { noremap = true, silent = true }, { desc = 'Switch to last buffer' })
+vim.keymap.set('n', '<Tab>', '<C-6>', { noremap = true }, { desc = 'Switch to last buffer' })
 vim.keymap.set("n", "Q", ":qa<CR>", { desc = "Quit all" })
 vim.keymap.set('n', '<leader>X', '<Cmd>bdelete<CR>', { desc = 'Close right buffer' })
 
