9068cd2d068b777e2b37b05df9f9e54fa17eb296
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2025-12-29 23:02:54 +0100

    automated dev commit

--- lua/viacheslav/plugins/statusline.lua
+++ lua/viacheslav/plugins/statusline.lua
@@ -3,33 +3,7 @@ return {
         "nvim-lualine/lualine.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function()
-            require("lualine").setup {
-                options = {
-                    section_separators = { left = '', right = '' },
-                    component_separators = { left = '', right = '' },
-                },
-                sections = {
-                    lualine_a = {
-                    },
-                    lualine_b = {
-                        'branch',
-                    },
-                    lualine_c = {
-                        'filename',
-                        {
-                            'diagnostics',
-                            right_padding = 0,
-                        },
-                    },
-                    lualine_x = {
-                        'diff',
-                    },
-                    lualine_y = {
-                        'filetype',
-                    },
-                    lualine_z = {},
-                },
-            }
+            require("lualine").setup {}
         end
     }
 }
