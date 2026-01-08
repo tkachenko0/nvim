fab4de74704d4025d1b8f7e331ac5849e0bda0bc
viacheslav.tkachenko <viacheslav.tkachenko@avvale.com>
2026-01-08 19:13:52 +0100

    automated dev commit

--- lua/viacheslav/core/remap.lua
+++ lua/viacheslav/core/remap.lua
@@ -5,18 +5,18 @@ vim.g.maplocalleader = ' '
 vim.keymap.set('n', ';', ':')
 vim.keymap.set('n', '\'', ';')
 
--- Remaps for beginning and end of line
-vim.keymap.set({ 'n', 'v' }, 'B', '_', { desc = 'Go to beginning of line' })
-vim.keymap.set({ 'n', 'v' }, 'E', '$', { desc = 'Go to end of line' })
+-- Line navigation
+vim.keymap.set({ 'n', 'v' }, 'B', '_')
+vim.keymap.set({ 'n', 'v' }, 'E', '$')
 
 -- Custom scripts
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
--- Move between windows
+-- Window navigation
 vim.keymap.set('n', '<C-Left>', '<C-w>h')
 vim.keymap.set('n', '<C-Down>', '<C-w>j')
 vim.keymap.set('n', '<C-Up>', '<C-w>k')
-vim.keymap.set('n', '<C-RIght>', '<C-w>l')
+vim.keymap.set('n', '<C-Right>', '<C-w>l')
 
 -- Quickfix navigation
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
@@ -25,13 +25,13 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 -- Clear highlights for search
 vim.keymap.set("n", "<esc>", "<cmd>nohlsearch<cr>", { desc = "Clear highlights" })
 
--- Move lines up and down
+-- Move lines
 vim.keymap.set('v', '<A-Down>', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', '<A-Up>', ":m '<-2<CR>gv=gv")
 vim.keymap.set('n', '<A-Down>', ':m .+1<CR>==')
 vim.keymap.set('n', '<A-Up>', ':m .-2<CR>==')
 
--- Vertical scroll and center
+-- Scroll & center
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
@@ -39,33 +39,29 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set('v', '<leader>s', '"hy:%s/<C-r>h/<C-r>h/g<Left><Left>')
 
--- Not copy when deleting
-vim.keymap.set({ "n", "v" }, "S", '"_S')
-vim.keymap.set({ "n", "v" }, "x", '"_x')
-vim.keymap.set({ "n", "v" }, "s", '"_s')
+-- Black-hole destructive actions
 vim.keymap.set({ "n", "v" }, "d", '"_d')
 vim.keymap.set({ "n", "v" }, "c", '"_c')
+vim.keymap.set({ "n", "v" }, "x", '"_x')
 
 -- Paste without overwriting the unnamed register
 vim.keymap.set("v", "p", '"_dP', { silent = true })
 
--- Go to beginning and end of the file
+-- File navigation
 vim.keymap.set({ "n", "v" }, "gg", "gg0")
 vim.keymap.set({ "n", "v" }, "G", "G$")
 
--- Stay in indent mode
+-- Indentation
 vim.keymap.set('v', '<', '<gv')
 vim.keymap.set('v', '>', '>gv')
 vim.keymap.set('n', '>', '>>')
 vim.keymap.set('n', '<', '<<')
 
--- Buffers
-vim.keymap.set("n", "<C-c>", "<cmd>%y+<CR>", { desc = "Copy the whole file" })
-vim.keymap.set("n", "<C-a>", "gg<S-v>G", { desc = "Select all" })
-vim.keymap.set('n', '<Tab>', '<C-6>', { desc = 'Switch to last buffer' })
+-- Buffers & selection
+vim.keymap.set("n", "<C-c>", "<cmd>%y+<CR>")
+vim.keymap.set("n", "<C-a>", "gg<S-v>G")
+vim.keymap.set('n', '<Tab>', '<C-6>')
 
--- Deletions
-vim.keymap.set("n", 'cw', '"_ciw')
-vim.keymap.set("n", 'dw', '"_diw')
+-- Text objects
 vim.keymap.set("n", 'yw', 'yiw')
 vim.keymap.set("n", 'vw', 'viw')
