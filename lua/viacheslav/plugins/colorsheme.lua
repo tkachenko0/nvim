86a3c6637f8ffd3b7455c4df22a53f9c0ccbe319
v.tkachenko <viacheslav.tkachenko@avvale.com>
2026-02-09 14:33:26 +0100

    automated dev commit

--- lua/viacheslav/plugins/colorsheme.lua
+++ lua/viacheslav/plugins/colorsheme.lua
@@ -8,7 +8,7 @@ return {
         -- Git diff colors (Visual Studio-like)
         vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#005a00", fg = "NONE" })
         vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#5f0000", fg = "NONE" })
-        vim.api.nvim_set_hl(0, "DiffChange", { bg = "#1B2A4A", fg = "NONE" })
+        vim.api.nvim_set_hl(0, "DiffChange", { bg = "#1d2433", fg = "NONE" })
         vim.api.nvim_set_hl(0, "DiffText", { bg = "#265478", bold = true })
     end,
 }
