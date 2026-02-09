66513a86b6204e6863a0a5ff6ab0a976b228a619
v.tkachenko <viacheslav.tkachenko@avvale.com>
2026-02-09 12:27:53 +0100

    automated dev commit

--- lua/viacheslav/plugins/treesitter.lua
+++ lua/viacheslav/plugins/treesitter.lua
@@ -9,7 +9,7 @@ return {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         config = function()
-            require("nvim-treesitter.configs").setup({
+            require("nvim-treesitter").setup({
                 ensure_installed = {
                     "vimdoc",
                     "javascript",
