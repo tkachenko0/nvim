6d4cf7a8c94ab5bbb904265442f38a8e37461401
v.tkachenko <viacheslav.tkachenko@avvale.com>
2026-02-09 12:41:48 +0100

    automated dev commit

--- lua/viacheslav/plugins/treesitter.lua
+++ lua/viacheslav/plugins/treesitter.lua
@@ -3,6 +3,29 @@ return {
     {
         "nvim-treesitter/nvim-treesitter-textobjects",
         dependencies = { "nvim-treesitter/nvim-treesitter" },
+        config = function()
+            require("nvim-treesitter-textobjects").setup({
+                select = {
+                    lookahead = true,
+                },
+            })
+
+            local select = require("nvim-treesitter-textobjects.select")
+            for _, mode in ipairs({ "x", "o" }) do
+                vim.keymap.set(mode, "af", function()
+                    select.select_textobject("@function.outer", "textobjects", mode)
+                end)
+                vim.keymap.set(mode, "if", function()
+                    select.select_textobject("@function.inner", "textobjects", mode)
+                end)
+                vim.keymap.set(mode, "ac", function()
+                    select.select_textobject("@class.outer", "textobjects", mode)
+                end)
+                vim.keymap.set(mode, "ic", function()
+                    select.select_textobject("@class.inner", "textobjects", mode)
+                end)
+            end
+        end,
     },
 
     {
@@ -34,18 +57,6 @@ return {
                     enable = true,
                 },
                 fold = { enable = true },
-                textobjects = {
-                    select = {
-                        enable = true,
-                        lookahead = true,
-                        keymaps = {
-                            ["af"] = "@function.outer",
-                            ["if"] = "@function.inner",
-                            ["ac"] = "@class.outer",
-                            ["ic"] = "@class.inner",
-                        },
-                    },
-                },
             })
         end,
     },
