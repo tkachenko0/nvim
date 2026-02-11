1b2d243f0d8284116427cac3fb165459e2a2b323
v.tkachenko <viacheslav.tkachenko@avvale.com>
2026-02-12 00:53:15 +0100

    automated dev commit

--- lua/viacheslav/plugins/statusline.lua
+++ lua/viacheslav/plugins/statusline.lua
@@ -5,17 +5,15 @@ return {
         require("lualine").setup {
             options = {
                 theme = "auto",
-                component_separators = '',
-                section_separators = { left = '', right = '' },
                 globalstatus = true,
             },
             sections = {
-                lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
-                lualine_b = { 'branch', 'diff' },
-                lualine_c = { 'filename', 'diagnostics' },
+                lualine_a = { 'branch' },
+                lualine_b = { "filename" },
+                lualine_c = { "diagnostics" },
                 lualine_x = {},
                 lualine_y = { 'filetype' },
-                lualine_z = { { 'location', separator = { right = '' }, left_padding = 2 } },
+                lualine_z = {},
             },
         }
     end
