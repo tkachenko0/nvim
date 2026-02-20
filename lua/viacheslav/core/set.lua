2777ddd3162f97c8ecc5533549c59f37d4566e85
v.tkachenko <viacheslav.tkachenko@avvale.com>
2026-02-20 18:01:07 +0100

    automated dev commit

--- lua/viacheslav/core/set.lua
+++ lua/viacheslav/core/set.lua
@@ -57,24 +57,3 @@ vim.diagnostic.config({
         border = "rounded"
     },
 })
-
-vim.api.nvim_create_user_command('Kiro', function(opts)
-    local question = opts.args
-    vim.cmd('split | terminal kiro-cli chat "' .. question .. '"')
-end, { nargs = 1 })
-
-vim.api.nvim_create_user_command('KiroSelection', function()
-    local start_pos = vim.fn.getpos("'<")
-    local end_pos = vim.fn.getpos("'>")
-    local lines = vim.fn.getline(start_pos[2], end_pos[2])
-
-    if #lines == 1 then
-        lines[1] = string.sub(lines[1], start_pos[3], end_pos[3])
-    else
-        lines[1] = string.sub(lines[1], start_pos[3])
-        lines[#lines] = string.sub(lines[#lines], 1, end_pos[3])
-    end
-
-    local text = table.concat(lines, '\n'):gsub('"', '\\"')
-    vim.cmd('split | terminal kiro-cli chat "' .. text .. '"')
-end, { range = true })
