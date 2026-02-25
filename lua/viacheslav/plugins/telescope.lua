01c8d1247a9200e89ed34580bff94108ed613ab4
v.tkachenko <t.viacheslav00@gmail.com>
2026-02-25 02:38:48 +0100

    icons

--- lua/viacheslav/plugins/telescope.lua
+++ lua/viacheslav/plugins/telescope.lua
@@ -11,7 +11,7 @@ return {
     { 'nvim-telescope/telescope-ui-select.nvim' },
     {
       'nvim-tree/nvim-web-devicons',
-      enabled = vim.g.have_nerd_font,
+      enabled = false,
     },
   },
   config = function()
