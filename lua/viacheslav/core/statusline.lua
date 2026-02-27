a1e2c983fb58308c256dfb057a5d9202c333db6e
v.tkachenko <t.viacheslav00@gmail.com>
2026-02-27 23:38:30 +0100

    feat(styling)

--- lua/viacheslav/core/statusline.lua
+++ lua/viacheslav/core/statusline.lua
@@ -29,13 +29,13 @@ end
 local function modified() return vim.bo.modified and ' [+]' or '' end
 
 local function lsp_status()
-  local clients = vim.lsp.get_clients({ bufnr = 0 })
+  local clients = vim.lsp.get_clients { bufnr = 0 }
   if #clients == 0 then return '' end
   local names = {}
   for _, client in ipairs(clients) do
     table.insert(names, client.name)
   end
-  return ' %#Type#[' .. table.concat(names, ',') .. ']%*'
+  return ' %#Type#[' .. table.concat(names, ', ') .. ']%*'
 end
 
 local function filetype() return vim.bo.filetype ~= '' and ' ' .. vim.bo.filetype or '' end
