66305fd8191301784d92f569061f0381dc69c6e8
v.tkachenko <t.viacheslav00@gmail.com>
2026-03-05 00:20:34 +0100

    dev commit

---
+++ lua/viacheslav/core/dashboard.lua
@@ -0,0 +1,36 @@
+local RECENT_FILES_COUNT = 3
+
+vim.api.nvim_create_autocmd('VimEnter', {
+  callback = function()
+    if vim.fn.argc() == 0 and vim.fn.expand('%') == '' then
+      local buf = vim.api.nvim_create_buf(false, true)
+      vim.api.nvim_set_current_buf(buf)
+
+      vim.bo[buf].buftype = 'nofile'
+
+      local cwd = vim.fn.getcwd()
+      local recent = {}
+
+      for _, file in ipairs(vim.v.oldfiles) do
+        if vim.startswith(file, cwd) and vim.fn.filereadable(file) == 1 then
+          table.insert(recent, file)
+          if #recent == RECENT_FILES_COUNT then break end
+        end
+      end
+
+      local lines = {}
+
+      for _, file in ipairs(recent) do
+        table.insert(lines, vim.fn.fnamemodify(file, ':~:.'))
+      end
+
+      vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+      vim.bo[buf].modifiable = false
+
+      vim.keymap.set('n', '<CR>', function()
+        local line = vim.api.nvim_win_get_cursor(0)[1]
+        if line <= #recent then vim.cmd('edit ' .. vim.fn.fnameescape(recent[line])) end
+      end, { buffer = buf })
+    end
+  end,
+})
--- lua/viacheslav/core/remap.lua
+++ lua/viacheslav/core/remap.lua
@@ -6,15 +6,8 @@ vim.keymap.set('n', '<C-f>', '<cmd>silent !tmux neww tmux-sessionizer<CR>')
 vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
 vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
 
-vim.keymap.set('n', '<esc>', '<cmd>nohlsearch<cr>')
-
 vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv")
-vim.keymap.set('n', '<A-j>', ':m .+1<CR>==')
-vim.keymap.set('n', '<A-k>', ':m .-2<CR>==')
-
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
 
 vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set('v', '<leader>s', '"hy:%s/<C-r>h/<C-r>h/g<Left><Left>')
@@ -29,8 +22,4 @@ vim.keymap.set('v', '>', '>gv')
 vim.keymap.set('n', '>', '>>')
 vim.keymap.set('n', '<', '<<')
 
-vim.keymap.set('n', '<C-a>', 'gg<S-v>G')
 vim.keymap.set('n', '<Tab>', '<C-6>')
-vim.keymap.set('n', '<C-c>', '<cmd>%y+<CR>')
-
-vim.keymap.set('n', 'Q', '<cmd>qa<CR>')
