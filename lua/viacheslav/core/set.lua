c356c66ab99019dbb8a066e49c4be555d0e52654
v.tkachenko <viacheslav.tkachenko@avvale.com>
2026-03-06 13:44:11 +0100

    automated dev commit

--- lua/viacheslav/core/set.lua
+++ lua/viacheslav/core/set.lua
@@ -7,6 +7,9 @@ vim.opt.scrolloff = 8
 vim.opt.termguicolors = true
 vim.opt.wrap = false
 
+vim.opt.swapfile = false
+vim.opt.backup = false
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
