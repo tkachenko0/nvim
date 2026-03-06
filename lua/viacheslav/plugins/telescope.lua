4eb6eb1279f827493d610f9dcfe09eb9c18364b0
v.tkachenko <viacheslav.tkachenko@avvale.com>
2026-03-06 14:12:18 +0100

    automated dev commit

--- lua/viacheslav/plugins/telescope.lua
+++ lua/viacheslav/plugins/telescope.lua
@@ -39,5 +39,6 @@ return {
     vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols)
     vim.keymap.set('n', '<leader>fS', builtin.lsp_dynamic_workspace_symbols)
     vim.keymap.set('n', '<leader>fg', builtin.live_grep)
+    vim.keymap.set('n', '<leader>fw', builtin.grep_string)
   end,
 }
