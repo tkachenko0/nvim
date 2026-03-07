5745050c6431c765f90c89a423b95745c25362ea
v.tkachenko <viacheslav.tkachenko@avvale.com>
2026-03-07 10:25:04 +0100

    feat(remaps)

--- lua/viacheslav/core/remap.lua
+++ lua/viacheslav/core/remap.lua
@@ -1,27 +1,33 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.keymap.set('n', '<C-f>', '<cmd>silent !tmux neww tmux-sessionizer<CR>')
+local set = vim.keymap.set
 
-vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
-vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
+-- set("n", "<C-j>", "<c-w><c-j>")
+-- set("n", "<C-k>", "<c-w><c-k>")
+-- set("n", "<C-l>", "<c-w><c-l>")
+-- set("n", "<C-h>", "<c-w><c-h>")
 
-vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv")
-vim.keymap.set('n', '<A-j>', ':m .+1<CR>==')
-vim.keymap.set('n', '<A-k>', ':m .-2<CR>==')
+set('n', '<C-k>', '<cmd>cnext<CR>zz')
+set('n', '<C-j>', '<cmd>cprev<CR>zz')
 
-vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set('v', '<leader>s', '"hy:%s/<C-r>h/<C-r>h/g<Left><Left>')
+set('v', '<A-j>', ":m '>+1<CR>gv=gv")
+set('v', '<A-k>', ":m '<-2<CR>gv=gv")
+set('n', '<A-j>', ':m .+1<CR>==')
+set('n', '<A-k>', ':m .-2<CR>==')
 
-vim.keymap.set('x', 'p', '"_dP')
-vim.keymap.set({ 'n', 'v' }, '<leader>p', '"+P')
-vim.keymap.set({ 'n', 'v' }, 'y', '"+y')
-vim.keymap.set({ 'n', 'v' }, '<leader>c', '"+c')
+set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+set('v', '<leader>s', '"hy:%s/<C-r>h/<C-r>h/g<Left><Left>')
 
-vim.keymap.set('v', '<', '<gv')
-vim.keymap.set('v', '>', '>gv')
-vim.keymap.set('n', '>', '>>')
-vim.keymap.set('n', '<', '<<')
+set('x', 'p', '"_dP')
+set({ 'n', 'v' }, '<leader>p', '"+P')
+set({ 'n', 'v' }, 'y', '"+y')
+set({ 'n', 'v' }, '<leader>c', '"+c')
 
-vim.keymap.set('n', '<Tab>', '<C-6>')
+set('v', '<', '<gv')
+set('v', '>', '>gv')
+set('n', '>', '>>')
+set('n', '<', '<<')
+
+set('n', '<Tab>', '<C-6>')
+set('n', '<C-f>', '<cmd>silent !tmux neww tmux-sessionizer<CR>')
