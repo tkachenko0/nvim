6939b45392499bfb0ce93ddf1866a253ebeb4909
v.tkachenko <viacheslav.tkachenko@avvale.com>
2026-03-10 18:47:48 +0100

    automated dev commit

--- lua/viacheslav/plugins/cmp.lua
+++ lua/viacheslav/plugins/cmp.lua
@@ -9,10 +9,6 @@ return {
       ['<Tab>'] = { 'select_next', 'fallback' },
       ['<S-Tab>'] = { 'select_prev', 'fallback' },
     },
-    appearance = {
-      use_nvim_cmp_as_default = true,
-      nerd_font_variant = 'mono',
-    },
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer' },
       per_filetype = {
