809b18eacf90cf4810e070dc7a27095aa04da50f
v.tkachenko <viacheslav.tkachenko@avvale.com>
2026-03-25 21:18:10 +0100

    automated dev commit

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,28 +1,11 @@
 return {
-  "rose-pine/neovim",
-  name = "rose-pine",
+  'Mofiqul/vscode.nvim',
   config = function()
-    require('rose-pine').setup({
-      styles = {
-        italic = false,
-      },
-    })
-    vim.cmd.colorscheme("rose-pine-moon")
+    require('vscode').setup {}
+    vim.cmd 'colorscheme vscode'
     vim.api.nvim_set_hl(0, 'DiffAdd', { bg = '#005a00', fg = 'NONE' })
     vim.api.nvim_set_hl(0, 'DiffDelete', { bg = '#5f0000', fg = 'NONE' })
     vim.api.nvim_set_hl(0, 'DiffChange', { bg = '#1d2433', fg = 'NONE' })
     vim.api.nvim_set_hl(0, 'DiffText', { bg = '#265478', bold = true })
-  end
+  end,
 }
-
--- return {
---   'Mofiqul/vscode.nvim',
---   config = function()
---     require('vscode').setup {}
---     vim.cmd 'colorscheme vscode'
---     vim.api.nvim_set_hl(0, 'DiffAdd', { bg = '#005a00', fg = 'NONE' })
---     vim.api.nvim_set_hl(0, 'DiffDelete', { bg = '#5f0000', fg = 'NONE' })
---     vim.api.nvim_set_hl(0, 'DiffChange', { bg = '#1d2433', fg = 'NONE' })
---     vim.api.nvim_set_hl(0, 'DiffText', { bg = '#265478', bold = true })
---   end,
--- }
