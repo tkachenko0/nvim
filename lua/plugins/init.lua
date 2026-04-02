e968c860d45ded532a1797ad4a65e10676998b0b
tkachenko0 <t.viacheslav00@gmail.com>
2026-04-02 21:33:13 +0200

    automated dev commit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,6 +17,8 @@ return {
     config = function()
       require('nvim-ts-autotag').setup {
         opts = {
+
+
           enable_close = true,
           enable_rename = true,
           enable_close_on_slash = true,
