f807028cf3ed973f09b9c46d0482bcd9a41cea76
tkachenko0 <t.viacheslav00@gmail.com>
2026-04-06 23:49:33 +0200

    automated dev commit

--- README.md
+++ README.md
@@ -3,7 +3,3 @@
 My personal Neovim setup.
 
 Part of my [Dotfiles repo](https://github.com/tkachenko0/dotfiles).
-
-## Inspirations
-
-- [ThePrimeagen's Neovim Config](https://github.com/ThePrimeagen/neovimrc)
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,8 +17,6 @@ return {
     config = function()
       require('nvim-ts-autotag').setup {
         opts = {
-
-
           enable_close = true,
           enable_rename = true,
           enable_close_on_slash = true,
