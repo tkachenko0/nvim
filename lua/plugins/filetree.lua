9a7071e3d2cde61e7630b36999cd429b54dde5b3
tkachenko0 <t.viacheslav00@gmail.com>
2026-06-08 12:28:26 +0200

    automated dev commit

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -8,6 +8,41 @@ return {
     cmd = 'Neotree',
     config = function()
         require('neo-tree').setup {
+            default_component_configs = {
+                -- expander ASCII al posto delle chevron del nerd font
+                indent = {
+                    with_expanders = true,
+                    expander_collapsed = '>',
+                    expander_expanded = 'v',
+                },
+                -- niente glifi del font per file/cartelle
+                icon = {
+                    folder_closed = '',
+                    folder_open = '',
+                    folder_empty = '',
+                    default = '',
+                    highlight = 'NeoTreeFileIcon',
+                },
+                -- "/" finale per indicare che è una cartella
+                name = {
+                    trailing_slash = true,
+                    use_git_status_colors = true,
+                },
+                -- stato git in versione testuale (no nerd font)
+                git_status = {
+                    symbols = {
+                        added = 'A',
+                        modified = 'M',
+                        deleted = 'D',
+                        renamed = 'R',
+                        untracked = '?',
+                        ignored = 'I',
+                        unstaged = 'U',
+                        staged = 'S',
+                        conflict = 'C',
+                    },
+                },
+            },
             window = {
                 mappings = {
                     ['<cr>'] = 'open',
